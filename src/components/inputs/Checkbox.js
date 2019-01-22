dc7fe6cf2e7c3b8b95d2a8a061438ea0fe07a554
deploy.sh <frederic.pun@gmail.com>
2019-01-21 22:55:54 -0500

    Fixes and checkbox

--- src/components/inputs/Checkbox.js
+++ src/components/inputs/Checkbox.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Check as Icon } from 'styled-icons/fa-solid/Check';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { PRIMARY_COLOUR } from '../variables';
@@ -6,47 +7,42 @@ import { PRIMARY_COLOUR } from '../variables';
 const Container = styled.div`
     overflow: hidden;
     position: relative;
-    border-radius: 999px;
     ${({ size }) => `
+        border-radius: ${ size * 0.3 }px;
         width: ${ size * 1.1 }px;
         height: ${ size * 1.1 }px;
     `}
 `;
 
-const Cover = styled.div`
-    border-radius: 999px;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    box-sizing: border-box;
+const StyledIcon = styled(Icon)`
+    color: white;
+    opacity: 0;
     transition:
-        border-color 300ms ease-in-out,
-        opacity 300ms ease-in-out
+        opacity 150ms ease-in-out,
+        transform 150ms ease-in-out
+    ;
+    transform: translate3d(0,0,0) scale(0) rotate(-45deg);
+    ${({ size }) => `
+        width: ${ size * 0.55 }px;
+        height: ${ size * 0.55 }px;
+    `}
+`;
+
+const Cover = styled.div`
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition:
+        background-color 150ms ease-in-out,
+        border-color 150ms ease-in-out
     ;
     ${({ size }) => `
-        border: ${ size / 12 }px solid #e6e6e6;
+        border-radius: ${ size * 0.3 }px;
+        border: ${ size * 0.1 }px solid #efefef;
+        width: ${ size * 1.1 }px;
+        height: ${ size * 1.1 }px;
     `}
-
-    &::after {
-        content: '';
-        border-radius: 999px;
-        ${({ size }) => `
-            height: calc(100% - ${ size / 5 }px);
-            width: calc(100% - ${ size / 5 }px);
-        `}
-
-        background-color: #e6e6e6;
-        transform: translate3d(0,0,0) scale(0);
-        opacity: 0;
-
-        transition:
-            background-color 300ms ease-in-out,
-            transform 300ms ease-in-out,
-            opacity 300ms ease-in-out
-        ;
-    }
 `;
 
 const Check = styled.input`
@@ -63,10 +59,10 @@ const Check = styled.input`
 
     &:checked ~ div {
         border-color: ${ PRIMARY_COLOUR };
-        &::after {
-            background-color: ${ PRIMARY_COLOUR };
-            transform: translate3d(0,0,0) scale(1);
+        background-color: ${ PRIMARY_COLOUR };
+        & > svg {
             opacity: 1;
+            transform: translate3d(0,0,0) scale(1) rotate(0);
         }
     }
 
@@ -75,32 +71,33 @@ const Check = styled.input`
     }
 `;
 
-export class Radio extends Component {
+export class Checkbox extends Component {
 
     state = { value: this.props.value }
     toggleState = () => this.setState(({ value }) => ({ value: !value }))
-    onChange = el => ( this.props.onChange ? this.props.onChange(el) : null )
 
     render() {
         const { value } = this.state;
-        const { className, size = 25, name, disabled } = this.props;
+        const { className, size = 25, onChange, name, disabled } = this.props;
         return (
             <Container size={size} disabled={disabled} className={className}>
                 <Check
                     name={name}
                     type='checkbox'
-                    value={value}
-                    onChange={this.onChange}
+                    checked={value}
+                    onChange={onChange}
                     onClick={this.toggleState}
                     disabled={disabled}
                 />
-                <Cover size={size}/>
+                <Cover size={size}>
+                    <StyledIcon size={size}/>
+                </Cover>
             </Container>
         );
     }
 };
 
-Radio.propTypes = {
+Checkbox.propTypes = {
     className: PropTypes.string,
     size: PropTypes.number,
     onChange: PropTypes.func,
--- src/stories/Radio.js
+++ src/stories/Radio.js
@@ -4,20 +4,20 @@ import { Radio } from '../components';
 
 storiesOf('Radio', module)
     .add('with nothing', () => (
-        <Radio/>
+        <Radio name='a'/>
     ), {
         notes: `The default size is 25. Some sizes cause positioning of dot to be off due to the accuracy of DOM`
     })
     .add('with size increased', () => (
-        <Radio size={400}/>
+        <Radio name='b' size={400}/>
     ))
     .add('with size decreased', () => (
-        <Radio size={4}/>
+        <Radio name='c' size={4}/>
     ), { notes: 'Might want to zoom in for this' })
     .add('with value', () => (
-        <Radio size={31} onChange={() => console.log('no')} value/>
+        <Radio name='d' size={31} onChange={({ target }) => alert(target.checked)} value/>
     ))
     .add('with disabled', () => (
-        <Radio size={31} value disabled/>
+        <Radio name='e' size={31} value disabled/>
     ))
 ;
