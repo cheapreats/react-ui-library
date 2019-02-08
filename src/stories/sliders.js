c661027610d40307741fedbc639e5ad4b5e028d6
Caroline <jingyi@advertience.com>
2019-02-07 19:16:14 -0500

    CHEAPEREATS-TICKET#CEM-16 add progresstion bar to react ui

--- src/stories/sliders.js
+++ src/stories/sliders.js
@@ -2,47 +2,29 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { BasicSlider } from '../components/sliders/sliders';
 import {PRIMARY_COLOUR} from "../components/variables";
-import { Bluetooth } from 'styled-icons/feather';
 
 const railStyle = {
     backgroundColor: PRIMARY_COLOUR
   };
 
-const trackStyle = {
-    backgroundColor: 'blue'
-}
-
-const handleStyle = {
-    borderColor: 'blue',
-    height: 28,
-    width: 28,
-    marginLeft: -14,
-    marginTop: -9,
-    backgroundColor: 'black',
-}
 storiesOf('BasicSlider', module)
     .add('Basic slider', () => (
-        <BasicSlider railStyle={railStyle}/>
+        <BasicSlider railStyle={railStyle} />
     ), {
         notes: `Basic Slider`
     })
     .add('Basic slider disabled', () => (
-        <BasicSlider disabled/>
+        <BasicSlider disabled railStyle={railStyle}/>
     ), {
         notes: `Basic Slider with handle fixed`
     })
     .add('Basic slider step with dots', () => (
-        <BasicSlider dots step={20} defaultValue={100} railStyle={railStyle}/>
+        <BasicSlider dots step={20} defaultValue={100} railStyle={railStyle} />
     ), {
         notes: `Basic Slider，step=20, dots`
     })
-    .add('Basic slider blue trackstyle', () => (
-        <BasicSlider trackStyle={trackStyle} />
+    .add('Basic slider red track', () => (
+        <BasicSlider />
     ), {
-        notes: `Basic Slider，blue track`
-    })
-    .add('Basic slider custom handle', () => (
-        <BasicSlider  handleStyle={handleStyle}/>
-    ), {
-        notes: `Basic Slider with custom handle`
+        notes: `Basic Slider，red track`
     });
