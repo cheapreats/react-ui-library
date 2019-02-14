060a91eafded8d7b5eb4921a5cff20d79dabf1ce
deploy.sh <frederic.pun@gmail.com>
2019-02-14 10:22:19 -0500

    Fixed footnote

--- src/components/containers/Footnote.js
+++ src/components/containers/Footnote.js
@@ -4,12 +4,12 @@ import { position, transition } from '../mixins';
 import { PRIMARY_FONT, SHADOW_RAISE_2 } from '../variables';
 
 const Container = styled.div`
-    ${ position('fixed', 'auto 0 0', 'auto', 0, 0, 0) }
+    ${ position('absolute', 'auto 0 0', 'auto', 0, 0, 0) }
     ${ transition(['transform', 'opacity']) }
     font-family: ${ PRIMARY_FONT };
     box-shadow: ${ SHADOW_RAISE_2 };
     padding: 20px;
-    ${ ({ show }) => !show ? `transform: translate3d(0, -100%, 0); opacity: 0; pointer-events: none;` : '' }
+    ${ ({ show }) => !show ? `transform: translate3d(0, 100%, 0); opacity: 0; pointer-events: none;` : '' }
 `;
 
 export const Footnote  = ({ className, children, show }) => (
