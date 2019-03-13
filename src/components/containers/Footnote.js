41e3cc9ac937ecde920044de9dd7dba9dbe5c7d6
fpunny <frederic.pun@gmail.com>
2019-03-13 15:28:09 -0400

    More footnote enhancement

--- src/components/containers/Footnote.js
+++ src/components/containers/Footnote.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
-import { position, transition, flex } from '../mixins';
+import { position, transition, scroll, flex } from '../mixins';
 import { PRIMARY_FONT, SHADOW_RAISE_2 } from '../variables';
 
 const Container = styled.div`
     ${ ({ pos }) => position(pos, 'auto 0 0', 'auto', 0, 0, 0) }
     ${ transition(['transform', 'opacity']) }
     ${ ({ f }) => flex(...f) }
-    font-family: ${ PRIMARY_FONT };
-    box-shadow: ${ SHADOW_RAISE_2 };
+    ${ scroll }
+
+    ${ ({ show }) => !show ? `transform: translate3d(0, 100%, 0); opacity: 0; pointer-events: none;` : '' }
     padding: ${ ({ padding }) => padding + (typeof(padding) === 'string' ? '' : 'px') };
     margin: ${ ({ margin }) => margin + (typeof(margin) === 'string' ? '' : 'px') };
-    ${ ({ show }) => !show ? `transform: translate3d(0, 100%, 0); opacity: 0; pointer-events: none;` : '' }
+    box-shadow: ${ SHADOW_RAISE_2 };
+    font-family: ${ PRIMARY_FONT };
+
     background-color: white;
+    box-sizing: border-box;
+    max-height: 100%;
+    max-width: 100%;
+    overflow: auto;
 `;
 
 export const Footnote  = ({
-    className,
-    children,
     flex = ['row', 'flex-start', 'center'],
     position = 'absolute',
-    margin = 0,
     padding = 20,
+    margin = 0,
+    className,
+    children,
     show
 }) => (
     <Container
