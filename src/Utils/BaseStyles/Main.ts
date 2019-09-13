ab69a4ab28a409907a2997e64e7f5ab706c26ff2
fpunny <frederic.pun@gmail.com>
2019-09-13 15:19:37 -0400

    Update basestyle main to have props for inline

--- src/Utils/BaseStyles/Main.ts
+++ src/Utils/BaseStyles/Main.ts
@@ -2,20 +2,25 @@
 export interface MainInterface {
     margin?: string | number;
     padding?: string | number;
-    inlineStyle?: string;
+    inlineStyle?: string | Function;
 }
 
 export const Main = ({
     margin = 0,
     padding = 0,
     inlineStyle = '',
+    ...props
 }: MainInterface): string => `
     // Positioning
     margin: ${margin};
     padding: ${padding};
 
     // Inline Styles
-    ${inlineStyle};
+    ${
+        typeof inlineStyle === 'function'
+            ? inlineStyle({ ...props, margin, padding })
+            : inlineStyle
+    };
 `;
 
 export const MainProps: string[] = ['margin', 'padding', 'inlineStyle'];
