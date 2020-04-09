c18067ae1778c56065e60505207fbf75a1ae7494
githubsahand <sahandfarahani@yahoo.com>
2020-04-09 16:12:09 -0400

    added card.tsx and index.ts

--- src/Containers/Card.tsx
+++ src/Containers/Card.tsx
@@ -11,6 +11,7 @@ import styled from 'styled-components';
 export interface CardProps {
     animated?: boolean;
     flat?: boolean;
+    maxWidth?:number;
 }
 
 export const Card: React.FC<CardProps> = ({
