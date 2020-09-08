5cdc658295caf77921c45286689b2e27d9a50466
ampatte2 <ampatte2@asu.edu>
2020-09-08 07:46:45 -0700

    removed imports in settingsSwitch stories

--- stories/Inputs/SettingsSwitch.stories.js
+++ stories/Inputs/SettingsSwitch.stories.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, boolean } from '@storybook/addon-knobs';
 import { SettingsSwitch} from '../../src';
 
 const switcher = (value) => console.log(value);
