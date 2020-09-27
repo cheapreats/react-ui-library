be992836ccc30ae7035fc8fdc136e1745d4cfcf2
eric-prog <eric.sprog@gmail.com>
2020-09-27 14:33:25 -0400

    display item

---
+++ stories/Containers/DisplayItem.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { DisplayItem, DisplayItemProps } from '../../src';
+import {createStoryTitle} from "../Constants";
+import { Meta, Story } from '@storybook/react';
+
+export default {
+    title: createStoryTitle('Display Item'),
+    component: DisplayItem,
+    args: {
+            label: 'DisplayItemLabel',
+            value: 'DisplayItemValue'
+    },
+} as Meta;
+
+export const Basic: Story<DisplayItemProps> = (args) => (
+           <DisplayItem {...args}></DisplayItem>
+);
