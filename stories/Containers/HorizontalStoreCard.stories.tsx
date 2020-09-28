bf6ab1d9363c315e672b9d94e5315344ee58e5cd
eric-prog <eric.sprog@gmail.com>
2020-09-27 23:02:28 -0400

    Horizontal Store Card

---
+++ stories/Containers/HorizontalStoreCard.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { HorizontalStoreCard, HorizontalStoreCardProps } from '../../src';
+import { Dog } from '@styled-icons/fa-solid/Dog';
+import {createStoryTitle} from "../Constants";
+import { Meta, Story } from '@storybook/react';
+
+export default {
+    title: createStoryTitle('Horizontal Store Card'),
+    component: HorizontalStoreCard,
+    args: {
+        image: "https://media.giphy.com/media/mCRJDo24UvJMA/giphy.gif",
+        pictureTags: [{ icon: Dog, text: 'Dog' }],
+        headerTags: ['$$', 'here', 'are', 'tags'],
+        tags: ['Free Shipping', 'Coupon', 'Coupon 2'],
+        rating: "4",
+        heading: "Hot Dog",
+        description: "this is a Horizontal Store Card",
+    },
+} as Meta;
+
+export const Basic: Story<HorizontalStoreCardProps> = (args) => (
+    <HorizontalStoreCard {...args}         
+    alt="coding Dog"
+    width="600px"
+    height="200px"
+/>
+);
