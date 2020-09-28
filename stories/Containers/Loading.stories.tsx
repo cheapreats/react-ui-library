ebf94bcf0cf2876e4a53f23d2051e19f66545dd9
eric-prog <eric.sprog@gmail.com>
2020-09-27 22:52:42 -0400

    Loading

---
+++ stories/Containers/Loading.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Loading, LoadingProps } from '../../src';
+import { createStoryTitle, getCaptionForLocale } from '../Constants';
+import { Meta, Story } from '@storybook/react';
+
+export default {
+    title: createStoryTitle('Loading'),
+    component: Loading,
+    args: {
+        loading: true,
+	    children: 'Boo!'
+    },
+} as Meta;
+
+export const Basic: Story<LoadingProps> = (args) => (
+   <Loading {...args}>{getCaptionForLocale(args.children)}</Loading>
+);
