6a0ea78895ede1c05e8360dcf2ec88796c28101a
eric-prog <eric.sprog@gmail.com>
2020-10-10 22:21:25 -0400

    Props

--- stories/Containers/RightSideBar.tsx
+++ stories/Containers/RightSideBar.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { RightSideBar } from '../../src';
+import { RightSideBar, RightSideBarProps } from '../../src';
 import { createStoryTitle } from '../Constants';
 import { Story, Meta } from '@storybook/react';
 
 export default {
-    title: createStoryTitle('RightSideBar'),
+    title: createStoryTitle('Right Side Bar'),
     component: RightSideBar,
     args: {
+        title: 'requirement',
+        checkboxArr: {'Name of Business': false}
     }
 } as Meta;
 
-export const Basic: Story = (args) => (
-    <RightSideBar 
-        {...args}  
-    />
+export const Basic: Story<RightSideBarProps> = (args) => (
+    <RightSideBar {...args}>Hi</RightSideBar>
 ); 
