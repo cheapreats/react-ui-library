84f6bf0ef0891fce75073dcfc38eb3826d288b53
eric-prog <eric.sprog@gmail.com>
2020-10-12 21:58:23 -0400

    update

--- src/Containers/RightSideBar/RightSideBar.tsx
+++ src/Containers/RightSideBar/RightSideBar.tsx
@@ -2,40 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import { Checkbox } from '../../Inputs/Checkbox';
 
-// 'Name of Business': false,
-// 'Business Address': true,
-// 'Contact Information': false,
-// 'Date when sales took place': false,
-// 'Time when sales took place': false,
-// 'Sales associate who rang up the sale': false,
-// 'Price for each product or service': false,
-// 'Sales tax rate(%)': false,
-// 'Amount tax': false,
-// 'Total price of sale': false,
-// 'Total price when tax included': false,
-// 'Quantity of each product or service': false,
-// 'Name of UPC of each product or service': false,
-// 'Station # of register where sale was transacted': false,
+export interface reqCheckbox {
+    label: string;
+    value: boolean
+}
 
 export interface RequirementProps {
     title: string;
-    checkboxArr: {
-        NAME_OF_BUSINESS: {
-            label: 'Name of Business'
-            value: false
-            name: 'checkbox'
-        },
-        BUSINESS_ADDRESS: {
-            label: 'Business Address'
-            value: false
-            name: 'checkbox'
-        },
-        CONTACT_INFORMATION: {
-            label: 'Contact Information'
-            value: false
-            name: 'checkbox'
-        }
-    }
+    checkboxArr: reqCheckbox[]
 }
 
 export const RightSideBar: React.FC<RequirementProps> = ({
@@ -55,7 +29,7 @@ export const RightSideBar: React.FC<RequirementProps> = ({
                     label={checkbox.label}
                     value={checkbox.value}
                     onChange={checkHandler}
-                    name={checkbox.name}
+                    name='checkbox'
                 />
             ))};
         </Wrapper>
