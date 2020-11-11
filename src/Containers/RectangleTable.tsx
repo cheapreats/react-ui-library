f4fd2146de5088dc9a6b3502df5f98483262b83e
dboichuk <dboichuk@mail.greenriver.edu>
2020-11-11 13:44:17 -0800

    added jsdocs

--- src/Containers/RectangleTable.tsx
+++ src/Containers/RectangleTable.tsx
@@ -33,212 +33,231 @@ enum occupancyStatusTypes {
 /**
  * Primary UI component for user interaction
  */
-
 export const RectangleTable: React.FC<IRectangleTable>
     = ({
-           tableID = 'T1',
-           numOfChairs = 4,
-           partyName = 'Null',
-           occupancyStatus = occupancyStatusTypes.Vacant,
-           reservationTime = Date.now(),
-           ...props
-       }) => {
+        tableID = 'T1',
+        numOfChairs = 4, 
+        partyName = 'Null',
+        occupancyStatus = occupancyStatusTypes.Vacant,
+        reservationTime = Date.now(),
+        ...props
+    }) => {
 
-    const {colors} = useTheme();
+        const {colors} = useTheme();
 
-    /**
-     * checks numOfChairs is between 1-6
-     * @param numOfChairs {number} uses the number of chairs
-     * @returns returns {number} of chairs of the component.
-     */
-    function getChairs(numOfChairs: number){
+        /**
+         * checks numOfChairs is between 1-6
+         * @param numOfChairs {number} uses the number of chairs
+         * @returns returns {number} of chairs of the component.
+         */
+        function getChairs(numOfChairs: number){
 
-        if(numOfChairs < 1 || numOfChairs > 6 ){
-            return 2;
-        }
+            if(numOfChairs < 1 || numOfChairs > 6 ){
+                return 2;
+            }
 
-        if(numOfChairs < 3){
-            return 2;
-        } else if (numOfChairs < 5){
-            return 4;
-        } else {
+            if(numOfChairs < 3){
+                return 2;
+            } if (numOfChairs < 5){
+                return 4;
+            } 
             return 6;
+        
         }
-    }
-    /**
-     * Primary function that returns the color from the occupancyStatus
-     */
-    function getOccupancyColor(status: occupancyStatusTypes) {
-        if (occupancyStatus === occupancyStatusTypes.Vacant) {
-            return colors.occupancyStatusColors.Vacant;
-        } if (occupancyStatus === occupancyStatusTypes.Reserved) {
-            return colors.occupancyStatusColors.Reserved;
+
+        /**
+         * This function will determine what color should be the Status and ColorDiv
+         * and return hexadecimal color value
+         * @param occupancyStatus {string} - Occupancy status
+         * @return {string} - Hexadecimal color value
+         */
+        function getOccupancyColor(status: occupancyStatusTypes) {
+
+            switch (occupancyStatus){
+            case occupancyStatusTypes.Vacant:
+                return colors.occupancyStatusColors.Vacant;
+
+            case occupancyStatusTypes.Reserved:
+                return colors.occupancyStatusColors.Reserved;
+
+            case occupancyStatusTypes.Occupied:
+                return colors.occupancyStatusColors.Occupied;
+            }
+
         }
-        return colors.occupancyStatusColors.Occupied;
 
-    }
+        return (
+            <StyledTable numOfChairs={getChairs(numOfChairs)}>
+                <div>
+                    <RowMargin0>
+                        {[...Array( (getChairs(numOfChairs) / 2) )].map((e, i) => (
+                            <Col key={i}>
+                                <Row>
+                                    <TopChair />
+                                </Row>
+                            </Col>
+                        ))}
+                    </RowMargin0>
+                </div>
 
-    return (
-        <StyledTable numOfChairs={ getChairs(numOfChairs) }>
-            <div>
-                <RowMargin0>
-                    {[...Array( (getChairs(numOfChairs) / 2) )].map((e, i) => (
-                        <Col key={i}>
-                            <Row>
-                                <TopChair />
-                            </Row>
-                        </Col>
-                    ))}
-                </RowMargin0>
-            </div>
+                <TableBody>
+                    <RowMargin0>
+                        <Col6P0>
+                            <RowM0H25>
+                                <Col6MxAutoMt3TxtLt>
+                                    {tableID}
+                                </Col6MxAutoMt3TxtLt>
+                            </RowM0H25>
+                            <RowM0H25 />
+                            <RowM0H50>
+                                <Col6MxAutoMt5>
+                                    <TextWhiteDiv>{partyName}</TextWhiteDiv>
+                                    <TextOccupancyColor occupancyColor={getOccupancyColor(occupancyStatus)} >
+                                        {occupancyStatus}
+                                    </TextOccupancyColor>
+                                </Col6MxAutoMt5>
+                            </RowM0H50>
+                        </Col6P0>
+                        <Col4P0 />
+                        <ColorBand occupancyColor={getOccupancyColor(occupancyStatus)} />
+                    </RowMargin0>
+                </TableBody>
+                <div>
+                    <RowMargin0>
+                        {[...Array( (getChairs(numOfChairs) / 2) )].map((e, i) => (
+                            <Col key={i}>
+                                <Row>
+                                    <BottomChair />
+                                </Row>
+                            </Col>
+                        ))}
+                    </RowMargin0>
+                </div>
+            </StyledTable>
+        );
+    };
 
-            <TableBody>
-                <RowMargin0>
-                    <Col6P0>
-                        <RowM0H25>
-                            <Col6MxAutoMt3TxtLt>
-                                {tableID}
-                            </Col6MxAutoMt3TxtLt>
-                        </RowM0H25>
-                        <RowM0H25 />
-                        <RowM0H50>
-                            <Col6MxAutoMt5>
-                                <TextWhiteDiv>{partyName}</TextWhiteDiv>
-                                <TextOccupancyColor
-                                occupancyColor={ getOccupancyColor(occupancyStatus) }
-                                >
-                                    {occupancyStatus}
-                                </TextOccupancyColor>
-                            </Col6MxAutoMt5>
-                        </RowM0H50>
-                    </Col6P0>
-                    <Col4P0 />
-                    <ColorBand   occupancyColor={ getOccupancyColor(occupancyStatus) } />
-                </RowMargin0>
-            </TableBody>
+/**
+ * variables for the styled components
+ */
+const StyledTable = styled.div`
+        
+        width: ${ ({numOfChairs}) => numOfChairs * 11}em;        
+`;
 
-            <div>
-                <RowMargin0>
-                    {[...Array( (getChairs(numOfChairs) / 2) )].map((e, i) => (
-                        <Col key={i}>
-                            <Row>
-                                <BottomChair />
-                            </Row>
-                        </Col>
-                    ))}
-                </RowMargin0>
-            </div>
-        </StyledTable>
-    );
-};
+const TopChair = styled.div`
+        
+        height: 3em;
+        border-top-left-radius: 3rem;
+        border-top-right-radius: 3rem;
+        flex: 0 0 50%;
+        max-width: 50%;
+        background-color: #6c757d;
+        margin: auto;
+`;
 
-    /**
-     * variables for the styled components
-     */
+const TableBody = styled.div`
+        
+        height: 22em;
+        border-radius: 3rem;
+        background-color: #6c757d;
+        margin: .25rem;
+`;
 
-    const StyledTable = styled.div`
-                width: ${ ({numOfChairs}) => numOfChairs * 11}em;        
-            `;
+const BottomChair = styled.div`
+        
+        height: 3em;
+        border-bottom-left-radius: 3rem;
+        border-bottom-right-radius: 3rem;
+        flex: 0 0 50%;
+        max-width: 50%;
+        background-color: #6c757d;
+        margin: auto !important;
+`;
 
-    const TopChair = styled.div`
-                height: 3em;
-                border-top-left-radius: 3rem;
-                border-top-right-radius: 3rem;
-                flex: 0 0 50%;
-                max-width: 50%;
-                background-color: #6c757d;
-                margin: auto;
-            `;
+const ColorBand = styled.div`
+            
+        border-top-right-radius: 3rem;
+        border-bottom-right-radius: 3rem;
+        height: 22em;
+        flex: 0 0 16.666667%;
+        max-width: 16.666667%;
+        position: relative;
+        width: 100%;
+        background-color: ${ ({occupancyColor}) => occupancyColor };
+`;
 
-    const TableBody = styled.div`
-                height: 22em;
-                border-radius: 3rem;
-                background-color: #6c757d;
-                margin: .25rem;
-                
-            `;
+const Row = styled.div`
+        
+        display: flex;
+        flex-wrap: wrap;
+        margin-right: 15px;
+        margin-left: 15px;
+`;
 
-    const BottomChair = styled.div`
-                height: 3em;
-                border-bottom-left-radius: 3rem;
-                border-bottom-right-radius: 3rem;
-                flex: 0 0 50%;
-                max-width: 50%;
-                background-color: #6c757d;
-                margin: auto !important;
-            `;
+const RowMargin0 = styled(Row)`
+            
+        margin: 0;  
+`;
 
-    const ColorBand = styled.div`
-            border-top-right-radius: 3rem;
-            border-bottom-right-radius: 3rem;
-            height: 22em;
-            flex: 0 0 16.666667%;
-            max-width: 16.666667%;
-            position: relative;
-            width: 100%;
-            background-color: ${ ({occupancyColor}) => occupancyColor };
-        `;
+const RowM0H25 = styled(RowMargin0)`
+        
+        height: 25%;  
+`;
 
-    const Row = styled.div`
-            display: flex;
-            flex-wrap: wrap;
-            margin-right: 15px;
-            margin-left: 15px;
-        `;
+const RowM0H50 = styled(RowMargin0)`
+        
+        height: 50%;  
+`;
 
-    const RowMargin0 = styled(Row)`
-            margin: 0;  
-        `;
+const Col = styled.div`
+            
+        flex-basis: 0;
+        flex-grow: 1;
+        max-width: 100%;
+`;
 
-    const RowM0H25 = styled(RowMargin0)`
-            height: 25%;  
-        `;
+const Col6P0 = styled.div`
+        
+        flex: 0 0 50%;
+        max-width: 50%;
+        position: relative;
+        width: 100%;
+`;
 
-    const RowM0H50 = styled(RowMargin0)`
-            height: 50%;  
-        `;
+const Col4P0 = styled.div`
+        
+        flex: 0 0 33.333333%;
+        max-width: 33.333333%;
+        position: relative;
+        width: 100%;
+`;
 
-    const Col = styled.div`
-            flex-basis: 0;
-            flex-grow: 1;
-            max-width: 100%;
-        `;
+const Col6MxAutoMt3TxtLt = styled(Col6P0)`
+        
+        padding-right: 15px;
+        padding-left: 15px;
+        color: #f8f9fa;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 1rem;
+`;
 
-    const Col6P0 = styled.div`
-            flex: 0 0 50%;
-            max-width: 50%;
-            position: relative;
-            width: 100%;
-        `;
+const Col6MxAutoMt5 = styled(Col6P0)`
+        
+        padding-right: 15px;
+        padding-left: 15px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 3rem;
+`;
 
-    const Col4P0 = styled.div`
-            flex: 0 0 33.333333%;
-            max-width: 33.333333%;
-            position: relative;
-            width: 100%;
-        `;
+const TextWhiteDiv = styled.div`
+            
+        color: #fff;
+`;
 
-    const Col6MxAutoMt3TxtLt = styled(Col6P0)`
-            padding-right: 15px;
-            padding-left: 15px;
-            color: #f8f9fa;
-            margin-left: auto;
-            margin-right: auto;
-            margin-top: 1rem;
-        `;
-
-    const Col6MxAutoMt5 = styled(Col6P0)`
-            padding-right: 15px;
-            padding-left: 15px;
-            margin-left: auto;
-            margin-right: auto;
-            margin-top: 3rem;
-        `;
-
-    const TextWhiteDiv = styled.div`
-            color: #fff;
-        `;
-
-    const TextOccupancyColor = styled.div`
-            color: ${ ({occupancyColor}) => occupancyColor };
-        `;
+const TextOccupancyColor = styled.div`
+            
+        color: ${ ({occupancyColor}) => occupancyColor };
+`;
--- src/Containers/SquareTable.tsx
+++ src/Containers/SquareTable.tsx
@@ -32,226 +32,235 @@ enum occupancyStatusTypes {
 
 /**
  * Primary UI component for user interaction
+ * Square Table
  */
-
 export const SquareTable: React.FC<ISquareTable>
     = ({
-           tableID = 'T1',
-           numOfChairs = 4,
-           partyName = 'Null',
-           occupancyStatus = occupancyStatusTypes.Vacant,
-           reservationTime = Date.now(),
-           ...props
-       }) => {
-    const chairNumOnSide= getChairNumOnSide(numOfChairs);
-    const {colors} = useTheme();
+        tableID = 'T1',
+        numOfChairs = 4,
+        partyName = 'Null',
+        occupancyStatus = occupancyStatusTypes.Vacant,
+        reservationTime = Date.now(),
+        ...props
+    }) => {
 
-    /**
-     * This function will calculate the number of chairs for each table side
-     * return 1 if numOfChairs is below 0
-     * returns the number of the chairs / 4 so it can be called on each side
-     */
+        const chairNumOnSide= getChairNumOnSide(numOfChairs);
+        const {colors} = useTheme();
 
-    function getChairNumOnSide(numOfChairs: number){
+        /**
+         * This function will determine how many chair to put per each side
+         * of the table (left, right, top, bottom)
+         * @param numOfChairs {number} - Total number of chairs per table
+         * @return {number} - Number of chair per table side
+         */
+        function getChairNumOnSide(numOfChairs: number){
 
-        if(numOfChairs < 1){
-            return 1;
-        }
-
-        if(numOfChairs%4==0){
-            return numOfChairs/4;
-        }
-        else{
+            if(numOfChairs < 1){
+                return 1;
+            }
+            if(numOfChairs%4===0){
+                return numOfChairs/4;
+            }
             return Math.floor(numOfChairs/4)+1;
-        }
-    }
-
-    /**
-     * This function will determine what color should be the Status and ColorDiv
-     */
-    function getOccupancyColor(status: occupancyStatusTypes) {
-        if (occupancyStatus === occupancyStatusTypes.Vacant) {
-            return colors.occupancyStatusColors.Vacant;
-        } if (occupancyStatus === occupancyStatusTypes.Reserved) {
-            return colors.occupancyStatusColors.Reserved;
-        }
-        return colors.occupancyStatusColors.Occupied;
-
-    }
-
-    return (
-        <div>
-            {/**chairs top*/}
-
-                <TopBottomRow chairNumOnSide={chairNumOnSide}>
-                    {[...Array(chairNumOnSide)].map((e,i) =>
-                    <ChairCol>
-                        <TopChair  key={i}/>
-                    </ChairCol>
-                    )}
-                </TopBottomRow>
-
             
-            {/**table itself*/}
+        }
+
+        /**
+         * This function will determine what color should be the Status and ColorDiv
+         * and return hexadecimal color value
+         * @param occupancyStatus {string} - Occupancy status
+         * @return {string} - Hexadecimal color value
+         */
+        function getOccupancyColor(status: occupancyStatusTypes) {
+
+            switch (occupancyStatus){
+            case occupancyStatusTypes.Vacant:
+                return colors.occupancyStatusColors.Vacant;
+                
+            case occupancyStatusTypes.Reserved:
+                return colors.occupancyStatusColors.Reserved;
+                    
+            case occupancyStatusTypes.Occupied:
+                return colors.occupancyStatusColors.Occupied;
+            }
+
+        }
+
+        return (
             <div>
-                <Row>
-
-                    {/**chairs left*/}
-                    <div>
-                        {[...Array(chairNumOnSide)].map((e,i) =>
-                            <SideChairRow>
-                                <LeftChair></LeftChair>
-                            </SideChairRow>
-                        )}          
-                    </div>
-
-                    <TableBody chairNumOnSide={chairNumOnSide}>
-                        <Row>
-                            <TableInfo>
-                                <p>
-                                    {tableID}<br></br>
-                                    {partyName}<br></br>
-                                    <Status
-                                    occupancyColor={getOccupancyColor(occupancyStatus)}
-                                    >{occupancyStatus}</Status><br></br>
-                                </p>
-                            </TableInfo>
-                            <ColorDiv chairNumOnSide={chairNumOnSide} occupancyColor={ getOccupancyColor(occupancyStatus)} />
-                        </Row>
-                    </TableBody>
-
-                    {/**chairs right*/}
-                    <div>
-                        {[...Array(chairNumOnSide)].map((e,i) =>
-                            <SideChairRow>
-                                <RightChair></RightChair>
-                            </SideChairRow>
-                        )}   
-                    </div>
-                </Row>
-            </div>
-
-            {/**chairs bottom*/}
-            <div>
-                <TopBottomRow chairNumOnSide={chairNumOnSide} >
-
-                {[...Array(chairNumOnSide)].map((e,i) =>
-
-                    <ChairCol>
-                        <BottomChair key={i} />
-                    </ChairCol>
-
+                {/** chairs top */}
+                <TopBottomRow chairNumOnSide={chairNumOnSide}>
+                    {[...Array(chairNumOnSide)].map((e,i) => (
+                        <ChairCol>
+                            {/* eslint-disable-next-line react/no-array-index-key */}
+                            <TopChair key={i} />
+                        </ChairCol>
+                    )
                     )}
                 </TopBottomRow>
-
+                {/** table itself */}
+                <div>
+                    <Row >
+                        {/** chairs left */}
+                        <div>
+                            {[...Array(chairNumOnSide)].map((e,i) => (
+                                <SideChairRow>
+                                    <LeftChair />
+                                </SideChairRow>
+                            )
+                            )}          
+                        </div>
+                        <TableBody chairNumOnSide={chairNumOnSide} >
+                            <Row>
+                                <TableInfo >
+                                    <p>
+                                        {tableID+"\n"+partyName }
+                                        <Status occupancyColor={getOccupancyColor(occupancyStatus)} >
+                                            {occupancyStatus}
+                                        </Status>
+                                    </p>
+                                </TableInfo>
+                                <ColorDiv chairNumOnSide={chairNumOnSide} occupancyColor={getOccupancyColor(occupancyStatus)} />
+                            </Row>
+                        </TableBody>
+                        {/** chairs right */}
+                        <div>
+                            {[...Array(chairNumOnSide)].map((e,i) => (
+                                <SideChairRow >
+                                    <RightChair />
+                                </SideChairRow>
+                            )
+                            )}   
+                        </div>
+                    </Row>
+                </div>
+                {/** chairs bottom */}
+                <div>
+                    <TopBottomRow chairNumOnSide={chairNumOnSide} >
+                        {[...Array(chairNumOnSide)].map((e,i) => (
+                            <ChairCol >
+                                <BottomChair key={i} />
+                            </ChairCol>
+                        )
+                        )}
+                    </TopBottomRow>
+                </div>
             </div>
+        );
+    };
 
-        </div>
-    );
-};
+/**
+ * Variables for the styled components
+ */
+const TableBody=styled.div`
+            
+        height: ${ ({chairNumOnSide}) => chairNumOnSide * 20}rem;
+        width: ${ ({chairNumOnSide}) => chairNumOnSide * 20}rem;
+        border-radius: 3rem;
+        background-color: #6c757d;
+`;
 
-    /**
-     * variables for the styled components
-     */
-    const TableBody=styled.div`
-            height: ${(chairNumOnSide) => chairNumOnSide * 20}rem;
-            width: ${({chairNumOnSide}) => chairNumOnSide * 20}rem;
-            border-radius: 3rem;
-            background-color: #6c757d;
-        `;
+const ColorDiv=styled.div`
+            
+        height: ${ ({chairNumOnSide}) => chairNumOnSide * 20}rem;
+        width: 3rem;
+        margin-left:auto;
+        margin-right: .95rem;
+        border-top-right-radius: 3rem;
+        border-bottom-right-radius: 3rem;
+        background-color: ${ ({occupancyColor}) => occupancyColor };
+`;
 
-    const ColorDiv=styled.div`
-            height: ${({chairNumOnSide}) => chairNumOnSide * 20}rem;
-            width: 3rem;
-            margin-left:auto;
-            margin-right: .95rem;
-            border-top-right-radius: 3rem;
-            border-bottom-right-radius: 3rem;
-            background-color: ${ ({occupancyColor}) => occupancyColor };
-            `;
-
-    const TopChair=styled.div`
-          border-top-left-radius: 3rem;
-          border-top-right-radius: 3rem;
-          height: 2rem;
-          width: 10rem;
-          margin-bottom: 0.25rem;
-          margin-left: auto;
-          margin-right: auto;
-          background-color: #6c757d;         
-        `;
-
-    const LeftChair=styled.div`
-          border-top-left-radius: 3rem;
-          border-bottom-left-radius: 3rem;
-          width: 2rem;
-          height: 10rem;
-          margin-top:auto;
-          margin-bottom: auto;
-          margin-right: 1.25rem;
-          margin-left: 1rem;
-          background-color: #6c757d;
-        `;
-
-    const SideChairRow=styled.div`
-           display: flex;
-           flex-wrap: wrap;
-           margin-right: -15px;
-           margin-left: -15px;
-           height: 20rem;
-        `;
-
-    const ChairCol=styled.div`
-          flex-basis: 0;
-          flex-grow: 1;
-          max-width: 100%;
+const TopChair=styled.div`
+          
+        border-top-left-radius: 3rem;
+        border-top-right-radius: 3rem;
+        height: 2rem;
+        width: 10rem;
+        margin-bottom: 0.25rem;
+        margin-left: auto;
+        margin-right: auto;
+        background-color: #6c757d;         
+ `;
     
-        `;
+const LeftChair=styled.div`
+          
+        border-top-left-radius: 3rem;
+        border-bottom-left-radius: 3rem;
+        width: 2rem;
+        height: 10rem;
+        margin-top:auto;
+        margin-bottom: auto;
+        margin-right: 1.25rem;
+        margin-left: 1rem;
+        background-color: #6c757d;
+`;
 
+const SideChairRow=styled.div`
+          
+        display: flex;
+        flex-wrap: wrap;
+        margin-right: -15px;
+        margin-left: -15px;
+        height: 20rem;
+`;
 
-    const RightChair=styled.div`
-            border-top-right-radius: 3rem;
-            border-bottom-right-radius: 3rem;
-            width: 2rem;
-            height: 10rem;
-            margin-top:auto;
-            margin-bottom: auto;
-            margin-left: 1.25rem;
-            background-color: #6c757d;
-        `;
+const ChairCol=styled.div`
+          
+        flex-basis: 0;
+        flex-grow: 1;
+        max-width: 100%;
+`;
 
-    const BottomChair=styled.div`
-          border-bottom-left-radius: 3rem;
-          border-bottom-right-radius: 3rem;
-          height: 2rem;
-          width: 10rem;
-          margin-top: 0.25rem;
-          margin-left: auto;
-          margin-right: auto;
-          background-color: #6c757d;
-         `;
+const RightChair=styled.div`
+          
+        border-top-right-radius: 3rem;
+        border-bottom-right-radius: 3rem;
+        width: 2rem;
+        height: 10rem;
+        margin-top:auto;
+        margin-bottom: auto;
+        margin-left: 1.25rem;
+        background-color: #6c757d;
+`;
 
-    const Row=styled.div`
-            display: flex;
-            flex-wrap: wrap;
-            margin-right: -15px;
-            margin-left: -15px;
-        `;
+const BottomChair=styled.div`
+         
+        border-bottom-left-radius: 3rem;
+        border-bottom-right-radius: 3rem;
+        height: 2rem;
+        width: 10rem;
+        margin-top: 0.25rem;
+        margin-left: auto;
+        margin-right: auto;
+        background-color: #6c757d;
+`;
 
-    const TableInfo=styled.div`
+const Row=styled.div`
+          
+        display: flex;
+        flex-wrap: wrap;
+        margin-right: -15px;
+        margin-left: -15px;
+`;
+
+const TableInfo=styled.div`
+        
         color: #f8f9fa;
         margin-top: 2rem;
         margin-left: 3rem;
+        white-space: pre-line;
+`;
+
+const TopBottomRow = styled.div`
+
+        display: flex;
+        flex-wrap: wrap;
+        width: ${ ({chairNumOnSide}) => chairNumOnSide * 20}rem;
+        margin-left: 1.3rem;
+`;
+
+const Status = styled.div`
         
-        `;
-
-    const TopBottomRow=styled.div`
-            display: flex;
-            flex-wrap: wrap;
-            width: ${({chairNumOnSide}) => chairNumOnSide * 20}rem;
-            margin-left: 1rem;
-        `;
-
-    const Status=styled.div`
-          color: ${ ({occupancyColor}) => occupancyColor };
-        `;
+        color: ${ ({occupancyColor}) => occupancyColor };
+`;
