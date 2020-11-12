bffc89d8dd01733ffc226a46da85b73471bbcbab
williancozzi <williancozzi@gmail.com>
2020-11-11 23:23:31 -0300

    converting DiningReservation from js to tsx

---
+++ stories/Containers/DiningReservation.stories.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import {
+    DiningReservation,
+    DiningReservationProps,
+} from '../../src/Containers/DiningReservation';
+import { createStoryTitle } from '../Constants';
+import { Meta, Story } from '@storybook/react';
+
+export default {
+    title: createStoryTitle('Dining Reservation'),
+    component: DiningReservation,
+} as Meta;
+
+export const Basic: Story<DiningReservationProps> = (args) => {
+    const modalState = useState(false);
+
+    return (
+        <DiningReservation
+            {...args}
+            modalState={modalState}
+        ></DiningReservation>
+    );
+};
