a2c199599efa38f26a332a5e9aaeb61c29b0a03b
williancozzi <williancozzi@gmail.com>
2020-11-14 21:26:46 -0300

    converted Datepicker story from js to tsx

---
+++ stories/Inputs/Datepicker.stories.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { Datepicker, DatepickerProps } from '../../src';
+import { createStoryTitle } from '../Constants';
+import { Meta, Story } from '@storybook/react';
+
+export default {
+    title: createStoryTitle('Datepicker'),
+    component: Datepicker,
+    argTypes: { onChange: { action: `you picked: ` } },
+    args: {
+        disabled: false,
+        placeholder: 'Placeholder',
+        description: 'Description',
+    },
+} as Meta;
+
+export const Basic: Story<DatepickerProps> = (args) => {
+    return <Datepicker {...args}></Datepicker>;
+};
