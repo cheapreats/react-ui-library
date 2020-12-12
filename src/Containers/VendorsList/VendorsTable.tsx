af9cb6002def330085d7b546fa7103a06d6d3a6d
xsharonhe <xsharonhe@gmail.com>
2020-12-12 16:24:25 -0500

    Preliminary table

--- src/Containers/VendorsList/VendorsTable.tsx
+++ src/Containers/VendorsList/VendorsTable.tsx
@@ -1,39 +1,63 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Profile, IProfileProps } from './Profile';
+import { useTable, Column } from 'react-table';
+import { IProfileProps } from './Profile';
 import { MainInterface, ResponsiveInterface } from '../../Utils/BaseStyles';
 import { flex, media } from '../../Utils/Mixins';
 
+export interface IVendorsData extends IProfileProps {
+    tags: string[];
+    createdAt: string;
+};
+
 export interface IVendorsTableProps extends MainInterface, ResponsiveInterface,React.HTMLAttributes<HTMLDivElement> {
-    data: IProfileProps[];
-    profileProps: IProfileProps;
+    data: IVendorsData[];
+    columns: Column<IVendorsData>[];
 };
 
 export const VendorsTable: React.FC<IVendorsTableProps> = ({
     data,
-    profileProps,
+    columns,
     ...props
 }): React.ReactElement => {
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow
+    } = useTable({
+        columns, 
+        data
+    });
+
     return (
-        <Wrapper {...props}>
-            {data.map(profile => (
-                <Profile
-                    {...profileProps}
-                    key={profile.key}
-                    name={profile.name}
-                    email={profile.email}
-                    imageUrl={profile?.imageUrl}
-                />
-            ))}
-        </Wrapper>
+        <table {...getTableProps()} {...props}>
+            <thead>
+                {headerGroups.map((headerGroup, index) => (
+                    <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.headers[index].Header?.toString()}>
+                        {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps()} key={column.Header?.toString()}>
+                                {column.render('Header')}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row) => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.getRowProps()} key={row.original.id}>
+                            {row.cells.map(cell => (
+                                <td {...cell.getCellProps()}>
+                                    {cell.render('Cell')}
+                                </td>
+                            ))}
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
     );
 };
-
-const Wrapper = styled.div`
-    ${flex('row')};
-    ${media(
-        'phone',
-        `
-        ${flex('column', 'center')};
-    `)};
-`;
--- stories/Containers/VendorsTable.stories.tsx
+++ stories/Containers/VendorsTable.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { VendorsTable, IVendorsTableProps } from '../../src';
+import styled from 'styled-components';
+import { VendorsTable, IVendorsTableProps, Profile, IProfileProps, SmallText, Mixins } from '../../src';
 import { createStoryTitle } from '../Constants';
 import { Story, Meta } from '@storybook/react';
 
@@ -8,18 +9,80 @@ export default {
     component: VendorsTable,
 } as Meta;
 
+const TagRowContainer = styled.div`
+    position: relative;
+    margin-top: auto;
+    ${Mixins.flex('space-evenly')}
+    flex-wrap: wrap;
+    top: 10%;
+`;
+const Tag = styled.div`
+    border-radius: 19px;
+    border-style: solid;
+    border-width: 2px;
+    text-align: center;
+    font-weight: bold;
+    font-size: 0.7em;
+    min-width: 24px;
+    ${({ theme }): string => `
+        border: 2px solid ${theme.colors.primary};
+        color: ${theme.colors.primary}
+    `};
+`;
+
 const getVendorsTableProps = () => ({
+    // columns and data must be memoized
+    columns: [
+        {
+            Header: 'Client',
+            accessor: 'col_client',
+            Cell: cell => (
+                <Profile 
+                    key={cell.row.original.id} 
+                    name={cell.row.original.name} 
+                    email={cell.row.original.email} 
+                    imageUrl={cell.row.original?.imageUrl} 
+                />
+            )
+        },
+        {
+            Header: 'Tags',
+            accessor: 'col_tags',
+            Cell: cell => (
+                <TagRowContainer>
+                    {cell.row.original.tags.map(
+                        (tag): React.ReactElement => (
+                            <Tag>{tag}</Tag>
+                        )
+                    )}
+                </TagRowContainer>
+            )
+        },
+        {
+            Header: 'Created At',
+            accessor: 'col_created',
+            Cell: cell => (
+                <SmallText>
+                    {cell.row.original.createdAt}
+                </SmallText>
+            )
+        }
+    ],
     data: [
         {
-            key: 1,
+            id: 1,
             name: 'Emy Jackson',
             email: 'emy_jac@upmind.com',
             imageUrl: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?cs=srgb&dl=pexels-pixabay-415829.jpg&fm=jpg',
+            tags: ['VIP Client', 'Early Adopter'],
+            createdAt: '24/05/2019'
         },
         {
-            key: 2,
+            id: 2,
             name: 'Amy Jackson',
-            email: 'amy_jac@upmind.com'
+            email: 'amy_jac@upmind.com',
+            tags: ['VIP Client'],
+            createdAt: '24/05/2019'
         }
     ]
 });
