d0089a4a0541f82aebd11f0c787f25bb00954ac3
roggc <nppcpp@hotmail.com>
2020-12-23 21:04:56 +0100

    adding event handlers as properties to the component

--- src/Containers/FileUpload/FileUpload.stories.tsx
+++ src/Containers/FileUpload/FileUpload.stories.tsx
@@ -11,6 +11,26 @@ color:LightBlue;
 text-decoration:none;
 `
 
+const processFiles=(event:React.DragEvent<HTMLDivElement>)=>{
+    event.preventDefault();
+    if(event.dataTransfer.items){
+        for (let i = 0; i < event.dataTransfer.items.length; i+=1) {
+            // If dropped items aren't files, reject them
+            if (event.dataTransfer.items[i].kind === 'file') {
+                const file = event.dataTransfer.items[i].getAsFile();
+                if(file){
+                // console.log(`... file[${i}].name = ${file.name}`);
+                }
+            }
+        }
+    }
+}
+
+const preventDefault=(event:Event | React.DragEvent<HTMLDivElement>)=>{
+    event.preventDefault();
+    event.stopPropagation();
+}
+
 export default {
     title: createStoryTitle('FileUpload'),
     component: FileUpload,
@@ -23,6 +43,8 @@ export default {
         <A href='http://google.com'>browse</A>
     </TextLayout>,
         minHeight:100,
+        onDrop:processFiles,
+        onDragOver:preventDefault,
     }
 } as Meta;
 
