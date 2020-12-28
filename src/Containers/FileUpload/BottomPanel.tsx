17ba6554e3801b8c438f84ed5e76e03ac5e7ac76
roggc <nppcpp@hotmail.com>
2020-12-28 16:24:17 +0100

    adding children to the interface

--- src/Containers/FileUpload/BottomPanel.tsx
+++ src/Containers/FileUpload/BottomPanel.tsx
@@ -2,6 +2,7 @@ import React,{forwardRef} from 'react'
 import {Container,IContainerProps} from './Container'
 
 interface IBottomPanelProps extends IContainerProps{
+    children:React.ReactElement;
 }
 
 export const BottomPanel=forwardRef<HTMLDivElement,IBottomPanelProps>(({children,...props},forwadedRef):React.ReactElement=>{
