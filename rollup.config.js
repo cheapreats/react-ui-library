ae6c12bafed51179b9f484dafcd37967259759ca
Andrew <ampatte2@asu.edu>
2021-01-08 13:23:58 -0700

    Working Version

--- rollup.config.js
+++ rollup.config.js
@@ -5,33 +5,42 @@ import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import css from 'rollup-plugin-css-only';
 import json from '@rollup/plugin-json';
+import { DEFAULT_EXTENSIONS } from '@babel/core';
+import pkg from './package.json'
 
 export default {
     input: 'src/index.ts',
     output: [
         {
-            dir: './dist',
-            format: 'esm',
+            name: '@cheapreats/react-ui',
+            file: pkg.main,
+            format: 'cjs',
+            sourcemap: true
+        },
+        {
+            file: pkg.module,
+            format: 'es'
         }
     ],
     plugins: [
-        tsPlugin({ typescript: ttypescript, tsconfigOverride: { compilerOptions : { module: "es2015" } }, tsconfigDefaults: {
+        commonjs(),
+        json(),
+        tsPlugin({ typescript: ttypescript, tsconfig: './tsconfig.json', include: [ "*.ts+(|x)", "**/*.ts+(|x)" ], tsconfigOverride: { compilerOptions : { module: "es2015" } }, tsconfigDefaults: {
             compilerOptions: {
                 plugins: [
                     { "transform": "@zerollup/ts-transform-paths" }
                 ]
             }
         } }),
+        nodeResolve({preferBuiltins: true}),
         babel({
-            exclude: 'node_modules/**',
-            extensions: ['.ts', '.tsx'],
+            exclude: ['node_modules'],
+            extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx'],
             plugins: ['babel-plugin-styled-components', 'babel-plugin-transform-class-properties'],
             presets: ['@babel/preset-env', '@babel/preset-react','@babel/preset-typescript']
         }),
-        css({output: 'bundle.css'}),
-        commonjs(),
-        json(),
-        nodeResolve({preferBuiltins: false}),
+        css()
+        
     ],
-    external: ['react', 'react-dom']
+    external: [...Object.keys(pkg.dependencies|| {}), ...Object.keys(pkg.peerDependencies|| {})]
 }
