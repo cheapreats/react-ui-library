62fbce2235c77f6049a5a7ba45657003e6d5dcdf
Your Name <william.fleckenstein@gmail.com>
2021-01-30 00:40:56 -0600

    Add Visit Notification

---
+++ src/Containers/VisitNotification/VisitNotification.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { Paragraph, SmallText } from '@Text';
+import { IVisitNotificationProps, VisitNotification } from '../../index';
+import { createStoryTitle } from '../../Constants';
+
+export default {
+    title: createStoryTitle('VisitNotification'),
+    component: VisitNotification,
+    args: {
+        imgSrc: 'https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI',
+        header: <Paragraph bold color="black">79 People</Paragraph>,
+        body: (
+            <Paragraph color="gray" bold>
+                Are viewing this page
+            </Paragraph>
+        ),
+        footer: (
+            <SmallText bold color="orange">
+                Verified by Nudgify
+            </SmallText>
+        ),
+    }
+} as Meta;
+
+export const Basic: Story<IVisitNotificationProps> = (args) => <VisitNotification {...args} />;
