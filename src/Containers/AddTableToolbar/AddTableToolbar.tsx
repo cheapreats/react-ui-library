c32ecf247ebd5089c7a04808eab146a7266d30da
Dmytro Boichuk <dboichuk@mail.greenriver.edu>
2021-03-16 13:19:21 -0700

    Updated the addTableToolbar to current version

--- src/Containers/AddTableToolbar/AddTableToolbar.tsx
+++ src/Containers/AddTableToolbar/AddTableToolbar.tsx
@@ -1,31 +1,194 @@
 import React from 'react';
-import Button from '@storybook/react/dist/demo/Button';
 import styled from 'styled-components';
+import {SquareTable,CircleTable} from '@Containers';
 
-export interface IAddTableToolbar {}
+
+export interface IAddTableToolbar {
+    /**
+     * on click function for Square table
+     */
+    onSquareTableClick: () => void;
+    /**
+     * on click function for Circle table
+     */
+    onCircleTableClick: () => void;
+    /**
+     * on click function for Rectangle table
+     */
+    onRectTableClick: () => void;
+}
 
 /**
  * Primary UI component for user interaction
  */
-export const AddTableToolbar: React.FC<IAddTableToolbar> = () => (
+export const AddTableToolbar: React.FC<IAddTableToolbar> = ({
+    onSquareTableClick, 
+    onCircleTableClick,
+    onRectTableClick,
+    ...props
+}) => (
     <BorderBox>
         <Container>
-            <Row>Add New Table</Row>
+            <Title>Add New Table</Title>
             <Row>
                 <Col>
                     <Center>
-                        <Button>Table 1</Button>
+                        <TableButton onClick={onSquareTableClick}>
+                            <SquareTable
+                                tableShape={'Square'}
+                                tableID="T1"
+                                partyName=""
+                                occupancyStatus="Vacant"
+                                chairs={[
+                                    {
+                                        position: 'top',
+                                        isSeated: true,
+                                        occupiedBy: 'Scott',
+                                        isVisible: true,
+                                        relativeSize: 0.3,
+                                        tableUse: 'TableForManagement',
+                                    },
+                                ]}
+                                isSquare={false}
+                                relativeSize={0.2}
+                                tableUse="AddTableButton"
+                            />
+                        </TableButton>
                     </Center>
-
                 </Col>
                 <Col>
                     <Center>
-                        <Button>Table 2</Button>
+                        <TableButton onClick={onCircleTableClick}>
+                            <CircleTable
+                                tableShape={'Circle'}
+                                tableID="T2"
+                                chairs={[
+                                    {
+                                        position: 'top',
+                                        isSeated: true,
+                                        occupiedBy: 'Scott',
+                                        isVisible: true,
+                                        isRound: true,
+                                        relativeSize: 0.2,
+                                        tableUse: 'TableForManagement',
+                                    },
+                                    {
+                                        position: 'top',
+                                        isSeated: false,
+                                        occupiedBy: '',
+                                        isVisible: true,
+                                        isRound: true,
+                                        relativeSize: 0.2,
+                                        tableUse: 'TableForManagement',
+                                    },
+                                    {
+                                        position: 'top',
+                                        isSeated: true,
+                                        occupiedBy: 'Dean',
+                                        isVisible: true,
+                                        isRound: true,
+                                        relativeSize: 0.2,
+                                        tableUse: 'TableForManagement',
+                                    },
+                                    {
+                                        position: 'top',
+                                        isSeated: false,
+                                        occupiedBy: '',
+                                        isVisible: true,
+                                        isRound: true,
+                                        relativeSize: 0.3,
+                                        tableUse: 'TableForManagement',
+                                    },
+                                    {
+                                        position: 'top',
+                                        isSeated: false,
+                                        occupiedBy: '',
+                                        isVisible: true,
+                                        isRound: true,
+                                        relativeSize: 0.3,
+                                        tableUse: 'TableForManagement',
+                                    },
+                                    {
+                                        position: 'top',
+                                        isSeated: false,
+                                        occupiedBy: '',
+                                        isVisible: true,
+                                        isRound: true,
+                                        relativeSize: 0.3,
+                                        tableUse: 'TableForManagement',
+                                    },
+                                ]}
+                                partyName=""
+                                occupancyStatus="Vacant"
+                                relativeSize={0.2}
+                                tableUse="AddTableButton"
+                            />
+                        </TableButton>
                     </Center>
                 </Col>
                 <Col>
                     <Center>
-                        <Button>Table 3</Button>
+                        <TableButton onClick={onRectTableClick}>
+                            <SquareTable
+                                tableShape={'Square'}
+                                tableID="T1"
+                                partyName=""
+                                occupancyStatus="Vacant"
+                                chairs={[
+                                    {
+                                        position: 'top',
+                                        isSeated: true,
+                                        occupiedBy: 'Scott',
+                                        isVisible: true,
+                                        relativeSize: 0.3,
+                                        tableUse: 'TableForManagement',
+                                    },
+                                    {
+                                        position: 'left',
+                                        isSeated: true,
+                                        occupiedBy: 'Scott',
+                                        isVisible: true,
+                                        relativeSize: 0.3,
+                                        tableUse: 'TableForManagement',
+                                    },
+                                    {
+                                        position: 'left',
+                                        isSeated: true,
+                                        occupiedBy: 'Scott',
+                                        isVisible: true,
+                                        relativeSize: 0.3,
+                                        tableUse: 'TableForManagement',
+                                    },
+                                    {
+                                        position: 'right',
+                                        isSeated: true,
+                                        occupiedBy: 'Scott',
+                                        isVisible: true,
+                                        relativeSize: 0.3,
+                                        tableUse: 'TableForManagement',
+                                    },
+                                    {
+                                        position: 'right',
+                                        isSeated: true,
+                                        occupiedBy: 'Scott',
+                                        isVisible: true,
+                                        relativeSize: 0.3,
+                                        tableUse: 'TableForManagement',
+                                    },
+                                    {
+                                        position: 'bottom',
+                                        isSeated: true,
+                                        occupiedBy: 'Scott',
+                                        isVisible: true,
+                                        relativeSize: 0.3,
+                                        tableUse: 'TableForManagement',
+                                    },
+                                ]}
+                                isSquare={false}
+                                relativeSize={0.2}
+                                tableUse="AddTableButton"
+                            />
+                        </TableButton>
                     </Center>
                 </Col>
             </Row>
@@ -36,7 +199,6 @@ export const AddTableToolbar: React.FC<IAddTableToolbar> = () => (
 /**
  * Styled component variables
  */
-
 const Container = styled.div`
     width: 90%;
     padding-right: 15px;
@@ -48,26 +210,39 @@ const Container = styled.div`
 const BorderBox = styled.div`
     border: 0.1em solid black;
     border-radius: 1em;
-    height: 11em;
-    width: 40em;
+    height: 210px;
+    width: 523px;
 `;
 
 const Row = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    margin-right: 15px;
-    margin-left: 15px;
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: 15px;
+  margin-left: 15px;
 `;
 
 const Col = styled.div`
-    flex-basis: 0;
-    flex-grow: 1;
-    width: 100%;
+  flex-basis: 0;
+  flex-grow: 1;
+  width: 100%;
 `;
 
 const Center = styled.div`
-  margin-top: 25%;
-  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  outline: none;
 `;
 
+const Title = styled.div`
+  font-size: 1.5rem;
+  font-family: Quicksand, sans-serif;
+`;
 
+const TableButton = styled.button`
+  border: none;
+  background: transparent;
+  outline: none;
+  cursor: pointer;
+`;
