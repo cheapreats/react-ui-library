28ac935e87ea57f0da0d03460b5358dbd0653bc7
roger <roggc9@gmail.com>
2021-03-31 16:19:33 +0200

    code quality 1

--- src/Containers/SankeyChart/SankeyChart.tsx
+++ src/Containers/SankeyChart/SankeyChart.tsx
@@ -6,6 +6,9 @@ import Theme from '../../Themes/ThemeTemplate'
 const LEGEND_CONTAINER_WIDTH=200
 const LEGEND_CONTAINER_HEIGHT=100
 const INITIAL_LEGEND_HEIGHT_OFFSET=10
+const LINK_COLOR='#77c878'
+const NODE_COLOR='#0088fe'
+const NODE_OPACITY=0.8
 
 export interface ISankeyChartProps{
     width:number;
@@ -43,7 +46,7 @@ export const SankeyChart:React.FC<ISankeyChartProps>=({
     return (
         <Sankey
             node={<MyCustomNode />}
-            link={{ stroke: '#77c878' }}
+            link={{ stroke: LINK_COLOR }}
             {...props}
             {...getProperties()}
         />
@@ -66,11 +69,25 @@ const MyCustomNode=({payload,...rest}:any):React.ReactElement<SVGElement>=>{
     useLayoutEffect(()=>{
         setHeightOffset(legendRef.current?.clientHeight)
     },[])
+
+    /**
+     * gets the x position for the top left corner of the container of the node legend, based on x property and dx,
+     * which is the node width
+     * @returns {number} the x position
+     */
+    const getLegendXPosition=()=>rest.x+payload.dx
+
+    /**
+     * gets the y position for the top left corner of the container of the node legend, based on y property, dy (which
+     * is the height of the node) and heighOffset, which is the height of the legend itself
+     * @returns {number} the y position
+     */
+    const getLegendYPosition=()=>rest.y+payload.dy/2-(heightOffset?heightOffset/2:INITIAL_LEGEND_HEIGHT_OFFSET)
     
     return (
         <svg>
-            <Rectangle className="recharts-sankey-node" fill="#0088fe" fillOpacity="0.8" payload={payload} {...rest} />
-            <Translate x={rest.x+payload.dx} y={rest.y+payload.dy/2-(heightOffset?heightOffset/2:INITIAL_LEGEND_HEIGHT_OFFSET)}>
+            <Rectangle className='recharts-sankey-node' fill={NODE_COLOR} fillOpacity={NODE_OPACITY} payload={payload} {...rest} />
+            <Translate x={getLegendXPosition()} y={getLegendYPosition()}>
                 <foreignObject width={LEGEND_CONTAINER_WIDTH} height={LEGEND_CONTAINER_HEIGHT}>
                     {renderLegend(payload.name,payload.value)}
                 </foreignObject>
@@ -79,7 +96,19 @@ const MyCustomNode=({payload,...rest}:any):React.ReactElement<SVGElement>=>{
     )
 }
 
-const Translate = ({x=0,y=0,children}:any) => {
+interface ITranslateProps{
+    x:number;
+    y:number;
+    children:React.ReactElement<SVGElement>;
+}
+
+
+/**
+ * it's a wrapper for a svg element to position it on x and y property values
+ * @param props {ITranslateProps} - the props passed to the component 
+ * @returns {React.ReactElement<SVGElement>} the positioned svg element
+ */
+const Translate = ({x=0,y=0,children}:ITranslateProps):React.ReactElement<SVGElement> => {
     if (!x && !y) 
         return children;
     return <g transform={`translate(${x},${y})`}>{children}</g>;
