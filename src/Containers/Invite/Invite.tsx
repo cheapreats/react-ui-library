b13d5c01f09df3bcec8d5aeb97ea041adb8c3f47
Brian Bantigue <bignatue@gmail.com>
2021-04-12 22:31:34 -0400

    Submit animation applied & property type specified

--- src/Containers/Invite/Invite.tsx
+++ src/Containers/Invite/Invite.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import Button from '@Inputs/Button/Button';
 import Input from '@Inputs/Input/Input';
 import Textarea from '@Inputs/Textarea/Textarea';
-import { flex, media, transition } from '@Utils/Mixins';
+import { flex, media } from '@Utils/Mixins';
 import { ChevronRight } from "@styled-icons/bootstrap/ChevronRight";
 import { ArrowRight } from "@styled-icons/bootstrap/ArrowRight";
 import { Heading, Paragraph, SmallText } from '../../index'; 
@@ -14,7 +14,12 @@ export interface InviteProps {
     description: string;
     footer: string;
     buttonText: string; 
-    inviteArgs: any[]; 
+    inviteArgs: {
+        label: string;
+        placeholder: string;
+        type: string;
+        subLabel?: string;
+    }[]; 
 }
 export const Invite: React.FC<InviteProps> = ({ 
     title,
@@ -22,8 +27,7 @@ export const Invite: React.FC<InviteProps> = ({
     description,
     footer,
     buttonText,
-    inviteArgs,
-    ...args
+    inviteArgs
 }) => {
     /**
      * @returns all elements of inviteArgs
@@ -36,7 +40,7 @@ export const Invite: React.FC<InviteProps> = ({
         return inviteArgs.map(
             (item, key ): React.ReactElement => (
                 (key !== lastKey) ? 
-                    <FormGroup key={item}>
+                    <FormGroup>
                         <Label>{item.label}</Label> 
                         <IDiv>
                             <Input 
@@ -46,13 +50,13 @@ export const Invite: React.FC<InviteProps> = ({
                         </IDiv>   
                     </FormGroup>                     
                     :      
-                    <FormGroup key={item}>              
+                    <FormGroup>              
                         <Label>
                             {item.label}
                             <Paragraph color="">{item.subLabel}</Paragraph>
                         </Label> 
                         <IDiv>
-                            <Textarea 
+                            <ITextarea 
                                 rows="3"
                                 placeholder={item.placeholder}
                                 value=""
@@ -64,38 +68,33 @@ export const Invite: React.FC<InviteProps> = ({
         )
     };
 
-    return (
-        <Wrapper>
-            <Row {...args}>
-                <Col>
-                    <Heading color="primary" type="h5" bold>{title}</Heading>
-                    <Heading type="h1" bold>{heading}</Heading>
-                    <Paragraph>{description}</Paragraph>
-                </Col>
-                <Col>  
-                    <Form>
-                        {formFields()}
-                        <FormButton>
-                            <IButton> 
-                                {buttonText}
-                                <IChevronRight />
-                                <IArrowRight />
-                            </IButton>
-                        </FormButton>
-                    </Form>
-                    <FormFooter>
-                        <SmallText >{footer}</SmallText>
-                    </FormFooter>
-                </Col>
-            </Row>
-        </Wrapper>
+    return ( 
+        <Row>
+            <Col>
+                <Heading color="primary" type="h5" bold>{title}</Heading>
+                <Heading type="h1" bold>{heading}</Heading>
+                <Paragraph>{description}</Paragraph>
+            </Col>
+            <Col>  
+                <Form>
+                    {formFields()} 
+                    <FormGroup> 
+                        <IButton> 
+                            {buttonText}
+                            <IChevronRight />
+                            <IArrowRight />
+                        </IButton> 
+                    </FormGroup> 
+                </Form>
+                <FormFooter>
+                    <SmallText >{footer}</SmallText>
+                </FormFooter>
+            </Col>
+        </Row> 
     );
 };
 
-/** styled */
-const Wrapper = styled.div`
-    padding: 1rem;
-`;
+/** styled */ 
 const Row = styled.div`
     margin-bottom: .6rem;
     ${flex('row')}
@@ -114,10 +113,13 @@ const Form = styled.form`
     border-radius: 5px;
     background-color: ${({ theme }) => theme.colors.occupancyStatusColors.Occupied};
 `;
-const FormGroup= styled.form`
+const FormGroup= styled.div`
     margin-bottom: .6rem;
-    ${flex('row')}
-    ${media('phone', 'flex-direction: column;')}
+    ${flex('row')};
+    &:last-child {
+        ${flex('flex-end')};
+    }
+    ${media('phone', 'flex-direction: column;')};
 `;
 const Label = styled.div`
     flex: 35%;
@@ -128,18 +130,25 @@ const IDiv = styled.div`
     width: 100%;
 `;
 const IChevronRight = styled(ChevronRight)` 
-    width: 12px; 
+    width: 12px;
     margin-left: 5px;
+    animation: SlideLeft .6s forwards;
+    @keyframes SlideLeft {        
+        0% {transform: translateX(5px);}
+        100% {transform: translateX(0px);}
+    }
 `;
 const IArrowRight = styled(ArrowRight)`
-    ${transition(['width', '1s'])}
     display: none; 
-    width: 0;
-    margin-left: -10px;
+    margin-left: 5px;
+    animation: SlideRight .6s forwards;
+    @keyframes SlideRight {        
+        0% {width: 5px;}
+        100% {width: 15px;}
+    }
 `;
-const FormButton = styled.div`
-    ${flex('row', 'flex-end')};
-    ${media('phone', 'justify-content: flex-start;')} 
+const ITextarea = styled(Textarea)`
+    min-height: 2.8rem;
 `;
 const IButton = styled(Button)`
     &:hover ${IChevronRight} {
@@ -147,7 +156,6 @@ const IButton = styled(Button)`
     }
     &:hover ${IArrowRight} {
         display: inline-block;
-        width: 15px; 
-        margin-left: 10px;
+        width: 15px;       
     }
 `;
