49b6e6ad62b33c51a86055ba21c074c3834926aa
Brian Bantigue <bignatue@gmail.com>
2021-05-06 00:38:38 -0400

    Clone from repo

--- src/Containers/Invite/Invite.stories.tsx
+++ src/Containers/Invite/Invite.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { createStoryTitle } from '../../Constants';
 import { Invite, InviteProps } from './Invite';
@@ -11,46 +11,45 @@ export default {
 const args = {
     title: 'Invite only',
     heading: 'Request an invitation to Stripe Treasury',
-    description: 'While Stripe Treasury is in initial roll-out, we’re inviting a limited number of platforms that serve US-based businesses to participate. If you’d like to request an invitation, please provide additional information and we’ll keep you updated.',
-    footer: 'Because Stripe Treasury is in beta, it is available to a limited number of invited platforms and capabilities will continue to change.', 
+    description:
+        'While Stripe Treasury is in initial roll-out, we’re inviting a limited number of platforms that serve US-based businesses to participate. If you’d like to request an invitation, please provide additional information and we’ll keep you updated.',
+    footer:
+        'Because Stripe Treasury is in beta, it is available to a limited number of invited platforms and capabilities will continue to change.',
     buttonText: 'Submit',
     inviteArgs: [
         {
-            name: "firstName",
+            name: 'firstName',
             label: 'First Name',
-            placeholder: 'Jane', 
-            type: "text"
+            placeholder: 'Jane',
+            type: 'text',
         },
         {
-            name: "lastName",
+            name: 'lastName',
             label: 'Last Name',
-            placeholder: 'Diaz', 
-            type: "text"
+            placeholder: 'Diaz',
+            type: 'text',
         },
         {
-            name: "email",
+            name: 'email',
             label: 'Work email',
-            placeholder: 'jane@account.com', 
-            type: "text"
+            placeholder: 'jane@account.com',
+            type: 'text',
         },
         {
-            name: "website",
+            name: 'website',
             label: 'Company website',
-            placeholder: 'example.com', 
-            type: "text"
+            placeholder: 'example.com',
+            type: 'text',
         },
         {
-            name:"otherInfo",
+            name: 'otherInfo',
             label: 'Other info',
-            subLabel: "Optional",
-            placeholder: 'What do you want...', 
-            type: "textarea"
+            subLabel: 'Optional',
+            placeholder: 'What do you want...',
+            type: 'textarea',
         },
     ],
 };
 
-export const Basic: Story<InviteProps> = (args) => (
-    <Invite {...args} />
-);
+export const Basic: Story<InviteProps> = (args) => <Invite {...args} />;
 Basic.args = { ...args };
-
