4015bafef50d7202a2ab65f6db9b7df21213f390
Kartikey <62225299+Kgg9@users.noreply.github.com>
2021-09-06 02:21:03 -0400

    Question Mark Hover React Component
    Created a new React Component

---
+++ src/Containers/QuestionMarkHover/QuestionMarkHover.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "styled-components";
+
+export interface QuestionMarkProps extends React.HTMLAttributes<HTMLDivElement> {
+    title: string
+}
+
+export const QuestionMarkHover: React.FC<QuestionMarkProps>=({title,...props})=> {
+    return <Section {...props}>
+    <QuestionMark>
+        ?
+    </QuestionMark>
+    <Hover>
+        <Rectangle> {title} </Rectangle>
+        <Triangle> </Triangle>
+    </Hover>
+ </Section>
+};
+
+const QuestionMark = styled.div `
+    font-size: 20px;
+    background: rgb(238 36 52);
+    color: white;
+    width: 25px;
+    border-radius: 50%;
+    text-align: center;
+    margin-top: 10px;
+`
+
+const Rectangle = styled.div`
+    display: flex;
+    height:50px;
+    width:225px;
+    background: white;
+    font-size: 12px;
+    justify-content:center;
+    align-items: center;
+    border: 2px solid white;
+    border-radius: 15px;
+    box-shadow: 0 0 40px -15px rgba(0,0,0,0.5);
+`;
+
+const Triangle = styled.div`
+    border-left: 20px solid transparent;
+    border-right: 20px solid transparent;
+    border-top: 20px solid white;
+    margin-top:-1px 
+    
+`;
+
+const Hover = styled.div`
+    visibility: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center; 
+`;
+
+const Section = styled.div`
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+
+    & ${QuestionMark}:hover + ${Hover} {
+        visibility: visible;
+     }
+`;
