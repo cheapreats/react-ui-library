3e11cfb907cba3437bc1fae47d36f7b5dc5a456b
chenj605 <jianjia.chen@mail.utoronto.ca>
2021-10-07 00:07:55 -0400

    added list to highlightedText

--- src/Containers/HighlightedText/HighlightedText.tsx
+++ src/Containers/HighlightedText/HighlightedText.tsx
@@ -3,6 +3,18 @@ import styled from 'styled-components';
 import { ClickableSmallText } from '@Text';
 import { Heading } from '@Text';
 import { flex } from '@Utils/Mixins';
+import { useArgs } from '@storybook/client-api';
+import { 
+    List,
+    ListFooter,
+    ListHeader,
+    ListHeaderProps,
+    ListItem,
+    ListProps,
+    ListToggle,
+} from '../../index';
+
+
 
 interface RowProps {
     padding?: string;
@@ -34,12 +46,65 @@ export const HighlightedText: React.FC<HighlightedTextProps> = ({
         console.log('hey')
     };
     
+    let items = [
+        { key: '1', data: 'data', date: 'today' },
+        { key: '2', data: 'data', date: 'yesterday' },
+        { key: '3', data: 'data', date: 'today' },
+        { key: '4', data: 'data', date: 'yesterday' },
+        { key: '5', data: 'data', date: 'today' },
+        { key: '7', data: 'data', date: 'yesterday' },
+        { key: '8', data: 'data', date: 'today' },
+        { key: '9', data: 'data', date: 'yesterday' },
+        { key: '10', data: 'data', date: 'today' },
+        { key: '11', data: 'data', date: 'yesterday' },
+        { key: '12', data: 'data', date: 'today' },
+    ];
+    let args = {
+        header: (
+            <ListHeader
+                label="Header"
+                headerFlex="space-between"
+                // icon={COG_WHEEL_ICON}
+                iconProps="width: 20px; margin-right: 10px;"
+                iconClick={() => alert('Icon Clicked')}
+            />
+        ),
+        footer: (
+            <ListFooter>
+                <p>This is a list Footer</p>
+            </ListFooter>
+        ),
+        // isOpen: false,
+        columnWidth: '300px',
+        loading: false,
+        cssPosition: 'string',
+        margin: '0',
+        left: '0',
+        right: 'auto',
+        id: '1',
+        mediaMixin: 'tablet',
+        mediaCssPosition: 'string',
+        mediaLeft: '0',
+        mediaRight: 'auto',
+        mediaMargin: 'auto',
+        mediaOnCloseTranslateXAxis: '-100%',
+    }
+
     return (
         <HighlightedRow padding={padding} width={width} {...props}>
             <p>
             We’re a <ClickableSmallText onClick={onClick}>Team</ClickableSmallText> of professionals working
             hard to provide free learning content.
             </p>
+            <List {...args}>
+                {items.map((item) => (
+                    <ListItem
+                        onClick={() => alert(`You clicked ${item.date}`)}
+                    >
+                        <p>{item.date}</p>
+                    </ListItem>
+                ))}
+            </List>
         </HighlightedRow>
     )
 };
