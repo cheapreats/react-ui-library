2ecea806b783fc9b7cc5eddab1a073be0c37b9af
chenj605 <jianjia.chen@mail.utoronto.ca>
2021-10-10 17:01:06 -0400

    added documentation

--- src/Containers/HighlightedText/HighlightedText.tsx
+++ src/Containers/HighlightedText/HighlightedText.tsx
@@ -7,11 +7,14 @@ import DropdownItem, { IDropdownItemProps } from '../Dropdown/DropdownItem';
 import { TextLayoutProps } from '../../Fragments/TextLayout';
 
 
-
 export interface HighlightedString {
+    /** Text: contents of the string */
     text: string;
+    /** IsSpecial: whether or not the string has options; if it has options, it will be bolded */
     isSpecial: boolean;
+    /** ItemProps: props for a dropdownItem representing an option */
     listItemsArgs?: Array<IDropdownItemProps>;
+    /** ItemBodies: contents of a dropdownItem representing an option */
     listItemsBodies?: Array<JSXElement>;
 }
 
@@ -19,13 +22,27 @@ export interface HighlightedTextProps extends TextLayoutProps {
     labels: Array<HighlightedString>;
 }
 
+/**
+ * A paragraph with special strings that can be clicked to reveal a dropdown list of more options
+ * @param labels - An array of HighlightedString; These will be displayed in the paragraph.
+ * @param props
+ * @constructor
+ */
 export const HighlightedText: React.FC<HighlightedTextProps> = ({
     labels,
     ...props
 }): React.ReactElement => {
 
+    /**
+     * construct the element for a special text
+     * @param label - HighlightedString of the special text
+     */
     const GetSpecialText = (label: HighlightedString): React.ReactElement => <ClickableSmallText bold>{`${label.text  } `}</ClickableSmallText>
 
+    /**
+     * construct the dropdown or text for a HighlightedString
+     * @param label - HighlightedString of the text
+     */
     const StyleSpecial = (label: HighlightedString): React.ReactElement => {
         if (label.isSpecial){
             const listItemsArgs: Array<IDropdownItemProps> = label.listItemsArgs || []
@@ -42,9 +59,11 @@ export const HighlightedText: React.FC<HighlightedTextProps> = ({
             </Dropdown>
         } 
         return <SmallText>{`${label.text  } `}</SmallText>
-        
     }
 
+    /**
+     * construct the elements from labels
+     */
     const RenderText = (): React.ReactElement => (
         <p>
             {labels.map((label) => (
