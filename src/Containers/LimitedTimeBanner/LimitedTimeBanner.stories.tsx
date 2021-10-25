56a1c791bba8933b6f824822777847a5e9c6d2f3
Hunter Fearnley <90480884+Hunterjdf@users.noreply.github.com>
2021-10-24 20:09:32 -0400

    Update LimitedTimeBanner.stories.tsx

--- src/Containers/LimitedTimeBanner/LimitedTimeBanner.stories.tsx
+++ src/Containers/LimitedTimeBanner/LimitedTimeBanner.stories.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { LimitedTimeBanner, BannerProps, Paragraph,} from '../../index';
-import { createStoryTitle, getCaptionForLocale } from '../../Constants';
-
+import { LimitedTimeBanner,LimitedTimeBannerProps} from '../../index';
+import { createStoryTitle} from '../../Constants';
 
 export default {
     title: createStoryTitle('LimitedTimeBanner'),
     component: LimitedTimeBanner,
     args: {
+        HoursRemaining: '2 Hours Remaining'
     },
 } as Meta;
 
-export const Basic: Story<BannerProps> = (args) => (
-    <LimitedTimeBanner {...args}>
-        <Paragraph bold>{getCaptionForLocale ('2 Hours Remaining')}</Paragraph>
-    </LimitedTimeBanner>
+export const Basic: Story<LimitedTimeBannerProps> = (args) => (
+    <LimitedTimeBanner {...args}/>
 );
