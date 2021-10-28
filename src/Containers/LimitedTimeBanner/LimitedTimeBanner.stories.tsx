b057035aaec08e6f695b69506d353e7653346643
Hunter Fearnley <90480884+Hunterjdf@users.noreply.github.com>
2021-10-27 22:48:51 -0400

    Update LimitedTimeBanner.stories.tsx

--- src/Containers/LimitedTimeBanner/LimitedTimeBanner.stories.tsx
+++ src/Containers/LimitedTimeBanner/LimitedTimeBanner.stories.tsx
@@ -7,7 +7,7 @@ export default {
     title: createStoryTitle('LimitedTimeBanner'),
     component: LimitedTimeBanner,
     args: {
-        HoursRemaining: '2 Hours Remaining'
+        HoursRemaining: 2, 
     },
 } as Meta;
 
