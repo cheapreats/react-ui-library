08892e8311d6d4fc262904be425df5b1aa0183f7
Madmanmayson <madmanmayson45@gmail.com>
2021-10-28 13:37:41 -0700

    Updated properties and added build functions

--- src/Containers/CRMTable/CRMTable.tsx
+++ src/Containers/CRMTable/CRMTable.tsx
@@ -3,8 +3,9 @@ import ReactTable from 'react-table';
 import styled from 'styled-components';
 
 export interface ICRMTableProps extends React.HTMLAttributes<HTMLTableElement> {
-    data: Object;
-    columns: Object;
+    // TODO: Replace 'Object' with the appropriate interfaces from the Header and Row Components when available.
+    data: Array<Object>; 
+    columns: Array<Object>;
 }
 
 export const CRMTable: React.FC<ICRMTableProps> = ({
@@ -12,19 +13,29 @@ export const CRMTable: React.FC<ICRMTableProps> = ({
     data,
     ...props
 }): React.ReactElement => {
-    const populateRow = () => "";
+    // The Header Component will be replaced with the Appropraite Header Component when available
+    const buildHeader = (columnsData: Array<Object>) => columnsData.map((column) => <Header {...column} />);
+    const buildRows = (rowsData: Array<Object>) => rowsData.map((row) => <Data {...row} />);
 
     return(
         <CRMMainTable {...props}>
             <CRMTableHeader>
-                {columns}
+                {buildHeader(columns)}
             </CRMTableHeader>
             <CRMTableBody>
-                {data}
+                {buildRows(data)}
             </CRMTableBody>
         </CRMMainTable>
     );
 }
+
+const Data = styled.tr<Object>`
+
+`;
+
+const Header = styled.th<Object>`
+
+`;
 
 const CRMMainTable = styled.table`
     border: 1px solid black;
