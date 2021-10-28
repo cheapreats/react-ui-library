a101ec3488750d4fae5da28bf03c956ac91bb29f
Ben Fuqua <Fuqua.Ben@student.greenriver.edu>
2021-10-28 14:07:39 -0700

    added basic structuring

--- src/Containers/CustomerProfile/CustomerProfile.tsx
+++ src/Containers/CustomerProfile/CustomerProfile.tsx
@@ -1,1 +1,20 @@
-export const CustomerProfile;
+import React from 'react';
+import styled from 'styled-components';
+
+export interface CustomerProfileProps extends React.HTMLAttributes<HTMLDivElement> {
+    profileName: string;
+    profileImage: string;
+}
+
+export const CustomerProfile: React.FC<CustomerProfileProps> = ({
+    profileName,
+    profileImage,
+    ...props
+})
+
+
+const ProfilePhoto = styled.img`
+    width: 50px;
+    height: 50px;
+    border-radius: 999px;
+`;
