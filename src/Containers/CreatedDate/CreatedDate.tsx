8fe137d5821f90919256b4bbb9a3af8e6666c157
George McMullen <sonofflynn32@gmail.com>
2021-10-28 20:24:50 -0700

    Created the CreatedDate component

--- src/Containers/CreatedDate/CreatedDate.tsx
+++ src/Containers/CreatedDate/CreatedDate.tsx
@@ -1,1 +1,30 @@
-export const CreatedDate;
+import React from 'react';
+import styled from 'styled-components';
+
+/* CONSTS */
+
+export interface CreatedDateProps extends React.HTMLAttributes<HTMLDivElement> {
+    /* Date of creation passed in */
+    dateCreated: String;
+}
+
+export const CreatedDate: React.FC<CreatedDateProps> = ({
+    /* Notification prop passed in */
+    dateCreated,
+    ...props
+}): React.ReactElement => (
+    <div {...props}>
+        <Date dateCreated={dateCreated}>
+            {dateCreated}
+        </Date>
+
+    </div>
+
+);
+
+const Date = styled.div<CreatedDateProps>`
+
+    ${({ theme }) => `
+        color:${theme.colors.text};
+    `}
+`;
