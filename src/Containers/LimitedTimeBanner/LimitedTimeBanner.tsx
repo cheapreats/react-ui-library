957c5e79780969c5b154308a2a559d5793e79986
Hunter Fearnley <90480884+Hunterjdf@users.noreply.github.com>
2021-11-02 21:24:09 -0400

    Update LimitedTimeBanner.tsx

--- src/Containers/LimitedTimeBanner/LimitedTimeBanner.tsx
+++ src/Containers/LimitedTimeBanner/LimitedTimeBanner.tsx
@@ -13,10 +13,25 @@ export const LimitedTimeBanner: React.FC<LimitedTimeBannerProps> = ({
 }): React.ReactElement => (
     <BannerBox {...props}>
         <Icon />
-        <p>{hoursRemaining} Hours Remaining</p>
+        <p>{hoursRemaining} Remaining</p>
     </BannerBox>
 ) ;
-
+const alterTime = (value:number)  => {
+    let newValue = value;
+    if(value <= 1){
+        return ('> ' + value) as string;
+    }
+    if(value >= 24){
+        newValue = Math.floor(value / 24) % 60;
+        value -= newValue * 24;
+        if(newValue>100){
+            return ('100 days +');
+        }
+        return (newValue + 'Days and '+ value + ' Hours')as string;
+    }
+    var str = value.toString();
+    return(str+' Hours');
+}
 const BannerBox = styled.div`
     ${({theme}):string => `
     font-family: ${theme.font.family};
