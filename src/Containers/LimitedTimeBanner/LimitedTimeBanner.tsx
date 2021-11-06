f412cd05db9e9e213e14a054ae748c38a16b5f79
Hunter Fearnley <90480884+Hunterjdf@users.noreply.github.com>
2021-11-06 14:46:47 -0400

    Update LimitedTimeBanner.tsx

--- src/Containers/LimitedTimeBanner/LimitedTimeBanner.tsx
+++ src/Containers/LimitedTimeBanner/LimitedTimeBanner.tsx
@@ -4,7 +4,7 @@ import {Clock} from '@styled-icons/bootstrap/Clock';
 import { MainInterface} from '@Utils/BaseStyles';
 import moment from 'moment';
 
-console.log(moment.now());
+moment.now();
 const MINUTES_IN_YEAR = 524160;
 
 /* Only importing the remaining time as a number  */
