7fa39ec9db8ed5d0d1f70470fc9057b1016d6544
Hunter Fearnley <90480884+Hunterjdf@users.noreply.github.com>
2021-11-07 14:11:39 -0500

    Update LimitedTimeBanner.stories.tsx

--- src/Containers/LimitedTimeBanner/LimitedTimeBanner.stories.tsx
+++ src/Containers/LimitedTimeBanner/LimitedTimeBanner.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { LimitedTimeBanner,LimitedTimeBannerProps} from '../../index';
+import { Tag, TagProps } from '../../index';
 import { createStoryTitle} from '../../Constants';
 
 export default {
