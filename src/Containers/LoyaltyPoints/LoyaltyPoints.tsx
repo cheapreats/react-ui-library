09b068c9412a98373f214440c81dc63463fa99b9
percypie <allertonnicholas9@gmail.com>
2021-11-08 23:28:34 -0500

    no longer using paragraph

--- src/Containers/LoyaltyPoints/LoyaltyPoints.tsx
+++ src/Containers/LoyaltyPoints/LoyaltyPoints.tsx
@@ -1,42 +1,40 @@
 import React from 'react'; 
 import styled from 'styled-components';
 import { Stars } from '@styled-icons/material/Stars';
-import {Paragraph} from '@Text';
 
 export interface LoyaltyPointsProps
     extends React.HTMLAttributes<HTMLDivElement>{
-            LoyaltyAmount: number;
-            LoyaltyPointLimit: number;
+            loyaltyamount: number; //the amount of loyalty points that the user gets with purchase, used to display on component.
+            loyaltypointlimit: number; //the limit of loyalty points before it says 99+ instead.
         }
 
 export const LoyaltyPoints: React.FC<LoyaltyPointsProps> = ({
-    LoyaltyAmount,
-    LoyaltyPointLimit, 
+    loyaltyamount,
+    loyaltypointlimit, 
     ...props
 }):React.ReactElement => <LoyaltyPointsBox {...props}>{ 
-    <Paragraph>+{getLoyaltyPoints(LoyaltyAmount,LoyaltyPointLimit)}<Star /></Paragraph>   
+    <p>+{getLoyaltyPoints(loyaltyamount,loyaltypointlimit)}<Star /></p>   
     }
 </LoyaltyPointsBox>;
 
 function getLoyaltyPoints(loyaltypoints: number,loyaltypointlimit: number){
     if(loyaltypoints >= loyaltypointlimit){
     return "99⁺";}
-    return Math.round(loyaltypoints)
+    return Math.round(loyaltypoints);
 }
 
 const LoyaltyPointsBox = styled.div`
-
     ${({theme, ...props}):string => `
         font-family: ${theme.font.family};
         font-size: 20px;
-        width: 60px;
+        width: 70px;
         height: 30px;
         white-space: wrap;
-        padding: 5px -10px;
         border-radius:0px 50px 50px 0px;
         background-color: ${theme.colors.background};
         color: ${theme.colors.loyaltyText};
         vertical-allign: middle;
+        text-allign: center;
     `}
 `
 const Star = styled(Stars)`
