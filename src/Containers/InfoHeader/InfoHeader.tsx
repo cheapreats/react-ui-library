e71ad8e9195d227bdcfd4c8f4eed05fef503b031
Hunter Fearnley <90480884+Hunterjdf@users.noreply.github.com>
2021-11-11 00:42:59 -0500

    Create InfoHeader.tsx

---
+++ src/Containers/InfoHeader/InfoHeader.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import {InfoCircleFill} from '@styled-icons/bootstrap/InfoCircleFill';
+
+export interface InfoHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
+    /* Text that will be shown */
+    Text: String;
+}
+
+export const InfoHeader: React.FC<InfoHeaderProps>  = ({
+    Text,
+    ...props
+}): React.ReactElement => (
+    <InfoHeaderStyle>
+    {Text} <Icon />
+</InfoHeaderStyle>
+);
+
+const InfoHeaderStyle = styled.header`
+text-align:left;
+padding-top: 8px;
+`;
+
+const Icon = styled(InfoCircleFill)`
+    width: 16px;
+    float: center;
+    padding-left: 8px;
+    padding-bottom: 5px;
+`;
+export default InfoHeader;
