57a4293ceaad93a0fa503e747338c115b3fe87cb
Madmanmayson <dylanhardy97@gmail.com>
2021-11-11 23:01:43 -0800

    Added start of SVG rendering logic

--- src/Containers/TreeAccordion/AccordionTier.tsx
+++ src/Containers/TreeAccordion/AccordionTier.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+
+const SVG_CONTAINER_WIDTH = 50;
+const SVG_HORIZONTAL_START = 10;
 
 export interface IAccordionTierProps extends React.HTMLAttributes<HTMLDivElement> {
     /* The header name for the tier */
     header: string;
+    children: Array<React.ReactNode>
 }
 
 export const AccordionTier: React.FC<IAccordionTierProps> = ({
@@ -11,7 +15,27 @@ export const AccordionTier: React.FC<IAccordionTierProps> = ({
     children,
     ...props
 }): React.ReactElement => {
-    const value = 0;
+    let totalChildrenHeight = 0;
+
+    const [path, setPath] = useState(<svg />);
+
+
+    const childrenRef = useRef<HTMLDivElement>(null);
+
+    useEffect((): void => {
+        const childrenContainer = childrenRef.current;
+        if(childrenContainer){
+            Array.from(childrenContainer.children).forEach((child) => {
+                totalChildrenHeight += child.clientHeight;
+            })
+        }
+
+        setPath(
+            <svg width={`${SVG_CONTAINER_WIDTH}px`} height={`${totalChildrenHeight}px`} xmlns="http://www.w3.org/2000/svg">
+                <path d={`M${SVG_HORIZONTAL_START} 0 V${totalChildrenHeight}`} stroke="black" fill="transparent"/>
+            </svg>
+        );
+    })
 
     return(
         <Tier {...props}>
@@ -20,9 +44,9 @@ export const AccordionTier: React.FC<IAccordionTierProps> = ({
             </HeaderContainer>
             <BodyContainer>
                 <SVGContainer>
-                    stuff
+                    {path}
                 </SVGContainer>
-                <ChildrenContainer>
+                <ChildrenContainer ref={childrenRef}>
                     {children}
                 </ChildrenContainer>
             </BodyContainer>
@@ -44,7 +68,9 @@ const BodyContainer = styled.div`
 `;
 
 const SVGContainer = styled.div`
-
+    ${() => `
+        width: ${SVG_CONTAINER_WIDTH}px;
+    `}
 `;
 
 const ChildrenContainer = styled.div`
