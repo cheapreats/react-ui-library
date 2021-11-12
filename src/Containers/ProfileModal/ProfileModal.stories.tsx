a0c51a7b8ddc5aa6b3c7ee970018cf75b041da7e
Mark Mendoza <mmendoza-rabara@mail.greenriver.edu>
2021-11-12 08:39:00 -0800

    optimized imports

--- src/Containers/ProfileModal/ProfileModal.stories.tsx
+++ src/Containers/ProfileModal/ProfileModal.stories.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { TagGroup } from '@Containers/TagGroup/TagGroup';
 import { ProfileModal, ProfileModalProps } from '../../index';
 import { createStoryTitle } from '../../Constants';
 
 export default {
     title: createStoryTitle('ProfileModal'),
-    component: TagGroup,
     args: {
         image: 'https://i0.wp.com/www.repol.copl.ulaval.ca/wp-content/uploads/2019/01/default-user-icon.jpg',
         name: 'Joshua',
         date: '02-08-1986',
-        tags: 'tags'
+        tags: 'tag-example'
     },
 } as Meta;
 
