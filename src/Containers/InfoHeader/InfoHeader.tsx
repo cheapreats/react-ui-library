0d17e74cb773d5c0c4f5d650470aefeddce7763f
Hunter Fearnley <90480884+Hunterjdf@users.noreply.github.com>
2021-11-18 20:59:35 -0500

    Update InfoHeader.tsx

--- src/Containers/InfoHeader/InfoHeader.tsx
+++ src/Containers/InfoHeader/InfoHeader.tsx
@@ -20,13 +20,12 @@ export const InfoHeader: React.FC<InfoHeaderProps>  = ({
 
 const InfoHeaderText = styled(Heading)`
     text-align:left;
-    padding-top: 0px;
 `;
 
 const Icon = styled(InfoCircleFill)`
     width: 20px;
     float: center;
-    padding: 0 0 12px 18px;
+    padding: 0 0 10px 20px;
 `;
 
 export default InfoHeader;
