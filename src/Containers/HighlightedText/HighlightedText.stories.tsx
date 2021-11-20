be2884fb43a0bd0b419f2f5f88dd0c269d0e6aef
Madmanmayson <madmanmayson45@gmail.com>
2021-11-19 20:14:13 -0800

    Restore changes from pull request #385
    https://github.com/cheapreats/react-ui-library/pull/385

--- src/Containers/HighlightedText/HighlightedText.stories.tsx
+++ src/Containers/HighlightedText/HighlightedText.stories.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import {
-    ClickableSmallText,
-    HighlightedString,
-    HighlightedText,
-    HighlightedTextProps,
-} from '../../index';
+import { ClickableSmallText, HighlightedString, HighlightedText, HighlightedTextProps } from '../../index';
 import { createStoryTitle } from '../../Constants';
 
 const labels: Array<HighlightedString> = [
@@ -19,12 +14,8 @@ const labels: Array<HighlightedString> = [
         isSpecial: true,
         listItemsArgs: [],
         listItemsBodies: [
-            <ClickableSmallText onClick={action('Burger Clicked.')}>
-                Burger
-            </ClickableSmallText>,
-            <ClickableSmallText onClick={action('Fries Clicked.')}>
-                Fries
-            </ClickableSmallText>,
+            <ClickableSmallText onClick={action('Burger Clicked.')}>Burger</ClickableSmallText>, 
+            <ClickableSmallText onClick={action('Fries Clicked.')}>Fries</ClickableSmallText>
         ],
     },
     {
@@ -32,19 +23,15 @@ const labels: Array<HighlightedString> = [
         isSpecial: false,
     },
     {
-        text: "Wendy's",
+        text: 'Wendy\'s',
         isSpecial: true,
         listItemsArgs: [],
         listItemsBodies: [
-            <ClickableSmallText onClick={action("Wendy's Clicked.")}>
-                {"Wendy's"}
-            </ClickableSmallText>,
-            <ClickableSmallText onClick={action('Burger King Clicked.')}>
-                Burger King
-            </ClickableSmallText>,
+            <ClickableSmallText onClick={action('Wendy\'s Clicked.')}>{'Wendy\'s'}</ClickableSmallText>, 
+            <ClickableSmallText onClick={action('Burger King Clicked.')}>Burger King</ClickableSmallText>
         ],
     },
-];
+]
 
 export default {
     title: createStoryTitle('Highlighted Text'),
@@ -57,10 +44,60 @@ export default {
             type: 'h2',
             padding: '0 0 0 5px',
             width: 200,
-        },
+        }
     },
 } as Meta;
 
-export const Basic: Story<HighlightedTextProps> = (args) => (
-    <HighlightedText {...args} />
-);
+export const Basic: Story<HighlightedTextProps> = (args) => <HighlightedText {...args} />;
+
+export const OldText = Basic.bind({});
+OldText.args = {
+    ...OldText.args,
+    labels:
+    [
+        {
+            text: 'Ordering a',
+            isSpecial: false,
+            age: 1,
+        },
+        {
+            text: 'Burger',
+            isSpecial: true,
+            listItemsArgs: [],
+            listItemsBodies: [
+                <ClickableSmallText onClick={action('Burger Clicked.')}>Burger</ClickableSmallText>, 
+                <ClickableSmallText onClick={action('Fries Clicked.')}>Fries</ClickableSmallText>
+            ],
+            age: 1,
+        },
+    ],
+};
+
+export const BothText = Basic.bind({});
+BothText.args = {
+    ...BothText.args,
+    labels:
+    [
+        {
+            text: 'Welcome to Burger Barn.',
+            isSpecial: false,
+            age: 1,
+        },
+        {
+            text: 'Would you like a ',
+            isSpecial: false,
+            age: 0,
+        },
+        {
+            text: 'Burger',
+            isSpecial: true,
+            listItemsArgs: [],
+            listItemsBodies: [
+                <ClickableSmallText onClick={action('Burger Clicked.')}>Burger</ClickableSmallText>, 
+                <ClickableSmallText onClick={action('Fries Clicked.')}>Fries</ClickableSmallText>
+            ],
+            age: 0,
+        },
+    ],
+};
+
--- src/Containers/HighlightedText/HighlightedText.tsx
+++ src/Containers/HighlightedText/HighlightedText.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import { ClickableSmallText, SmallText } from '@Text';
 import Dropdown, { IDropdownProps } from '../Dropdown/Dropdown';
 import DropdownItem, { IDropdownItemProps } from '../Dropdown/DropdownItem';
 import { TextLayoutProps } from '../../Fragments/TextLayout';
+
+const newTextOpacity = 1;
+const oldTextOpacity = 0.5;
 
 export interface HighlightedString {
     /** contents of the string */
@@ -18,11 +21,12 @@ export interface HighlightedString {
     listItemsArgs?: Array<IDropdownItemProps>;
     /** content of DropdownItems */
     listItemsBodies?: Array<JSX.Element>;
+    /** in [0, 1]; 0 for newest (opaque), 1 for oldest (most transparent). None for automatic */
+    age?: number;
 }
 
 // extends line div
-export interface HighlightedTextProps
-    extends React.HTMLAttributes<HTMLParagraphElement> {
+export interface HighlightedTextProps extends React.HTMLAttributes<HTMLParagraphElement> {
     labels: Array<HighlightedString>;
 }
 
@@ -36,53 +40,70 @@ export const HighlightedText: React.FC<HighlightedTextProps> = ({
     labels,
     ...props
 }): React.ReactElement => {
+    const [numEntries, setNumEntries] = useState(0);
+    const [numNewEntries, setNumNewEntries] = useState(0);
+
+    useEffect(() => {
+        if (labels.length !== numEntries) {
+            setNumNewEntries (labels.length - numEntries)
+            setNumEntries(labels.length)
+        }
+    })
+
     /**
      * construct the element for a special text
      * @param label - HighlightedString of the special text
      */
-    const getSpecialTextComponent = (
-        label: HighlightedString,
-    ): React.ReactElement => (
-        <ClickableSmallText
-            bold
-            {...label.textProps}
-        >{`${label.text} `}</ClickableSmallText>
-    );
+    const getSpecialTextComponent = (label: HighlightedString, opacity: number): React.ReactElement => <ClickableSmallText style={{opacity}} bold {...label.textProps}>{`${label.text  } `}</ClickableSmallText>
 
     /**
      * construct the dropdown or text for a HighlightedString
      * @param label - HighlightedString of the text
      */
-    const getTextComponent = (label: HighlightedString): React.ReactElement => {
-        if (label.isSpecial) {
-            const listItemsArgs: Array<IDropdownItemProps> =
-                label.listItemsArgs || [];
-            const listItemsBodies: Array<JSX.Element> =
-                label.listItemsBodies || [];
-            const DropDownProps: IDropdownProps = {
-                dropdownButton: getSpecialTextComponent(label),
-            };
-            return (
-                <Dropdown {...DropDownProps} {...label.listProps}>
-                    {listItemsBodies.map((_, index) => (
-                        <DropdownItem {...listItemsArgs[index]}>
-                            {listItemsBodies[index]}
-                        </DropdownItem>
-                    ))}
-                </Dropdown>
-            );
+    const getTextComponent = (label: HighlightedString, index: number): React.ReactElement => {
+        let opacity: number;
+        if (label.age != null){
+            opacity = newTextOpacity - label.age*oldTextOpacity
+        } else if (index >= labels.length - numNewEntries) {
+            opacity = newTextOpacity
+        } else {
+            opacity = oldTextOpacity
         }
-        return <SmallText {...label.textProps}>{`${label.text} `}</SmallText>;
-    };
+        
+        if (label.isSpecial){
+            const listItemsArgs: Array<IDropdownItemProps> = label.listItemsArgs || []
+            const listItemsBodies: Array<JSX.Element> = label.listItemsBodies || []
+            const DropDownProps: IDropdownProps = {
+                dropdownButton: getSpecialTextComponent(label, opacity),
+            }
+            return <Dropdown {...DropDownProps} {...label.listProps}>
+                {listItemsBodies.map((_, i) => (
+                    <DropdownItem {...listItemsArgs[i]}>
+                        {listItemsBodies[i]}
+                    </DropdownItem>
+                ))}
+            </Dropdown>
+        } 
+        return <SmallText style={{opacity}} {...label.textProps}>{`${label.text  } `}</SmallText>
+    }
 
     /**
      * construct elements from labels
      */
     const renderText = (): React.ReactElement => (
-        <p>{labels.map((label) => getTextComponent(label))}</p>
-    );
+        <p>
+            {labels.map((label, index) => (
+                getTextComponent(label, index)
+            ))}
+        </p>
+    )
 
-    return <HighlightedRow {...props}>{renderText()}</HighlightedRow>;
+    return (
+        <HighlightedRow {...props}>
+            {renderText()}
+        </HighlightedRow>
+    )
 };
+
 
 const HighlightedRow = styled.div``;
