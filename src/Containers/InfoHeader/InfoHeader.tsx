e7e1e74f2fc5b15ab50de5b16614463e8b78b088
Hunter Fearnley <90480884+Hunterjdf@users.noreply.github.com>
2021-11-21 18:44:05 -0500

    Update InfoHeader.tsx

--- src/Containers/InfoHeader/InfoHeader.tsx
+++ src/Containers/InfoHeader/InfoHeader.tsx
@@ -3,11 +3,17 @@ import styled from 'styled-components';
 import {InfoCircleFill} from '@styled-icons/bootstrap/InfoCircleFill';
 import {Heading, HeadingProps} from '@Text';
 
-const [isShown, setIsShown] = useState(false);
-
+// 
+/*
+        onMouseEnter={() => setIsShown(false)}
+        onMouseLeave={() => setIsShown(true)}
+        {isShown && (  
+                    )} 
+             */
 export interface InfoHeaderProps extends HeadingProps {
     /* Text that will be displayed to the left of the icon */ 
     infotext: String;
+    
 }
 
 export const InfoHeader: React.FC<InfoHeaderProps>  = ({
@@ -15,14 +21,31 @@ export const InfoHeader: React.FC<InfoHeaderProps>  = ({
     ...props
 }): React.ReactElement => (
     <InfoHeaderText {...props}>
-        {infotext}
-        onMouseEnter={() => setIsShown(false)}
-        onMouseLeave={() => setIsShown(true)}
-        {isShown && (    
-        <Icon />
-        )}
+        {showIcon(infotext)}
     </InfoHeaderText>
 );
+
+/**
+* This function takes in infotext and determines if the user is mousing over it 
+* If the user is the funtion will return the icon as well  as the text 
+* Otherwise just the text is shown 
+* @param {String} text - The time remaining in minutes  
+* @returns {InfoHeaderText} - The text and icon  
+*/
+function showIcon(text:String) {
+    const [isShown, setIsShown] = useState(false);
+    
+    return (
+        <InfoHeaderText
+          onMouseEnter={() => setIsShown(true)}
+          onMouseLeave={() => setIsShown(false)}>
+          {text}
+          {isShown && (
+            <Icon />
+        )}
+        </InfoHeaderText>
+    );
+  }
 
 const InfoHeaderText = styled(Heading)`
     text-align: left;
