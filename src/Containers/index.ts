c0b405bc1943f2f88355010635692c0fe9e542df
Hunter Fearnley <90480884+Hunterjdf@users.noreply.github.com>
2021-11-20 19:34:46 -0500

    Update index.ts

--- src/Containers/index.ts
+++ src/Containers/index.ts
@@ -90,4 +90,5 @@ export * from './CreatedDate/CreatedDate';
 export * from './CRMRow/CRMRow';
 export * from './CRMTable/CRMTable';
 export * from './LimitedTimeBanner/LimitedTimeBanner';
+export * from './InfoHeader/InfoHeader';
 
