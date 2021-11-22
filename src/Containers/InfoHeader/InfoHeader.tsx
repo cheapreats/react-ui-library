227e9e13ae3bef142b82de9134a16ad262b39aba
Hunter Fearnley <90480884+Hunterjdf@users.noreply.github.com>
2021-11-22 01:23:53 -0500

    Update InfoHeader.tsx

--- src/Containers/InfoHeader/InfoHeader.tsx
+++ src/Containers/InfoHeader/InfoHeader.tsx
@@ -3,13 +3,6 @@ import styled from 'styled-components';
 import {InfoCircleFill} from '@styled-icons/bootstrap/InfoCircleFill';
 import {Heading, HeadingProps} from '@Text';
 
-// 
-/*
-        onMouseEnter={() => setIsShown(false)}
-        onMouseLeave={() => setIsShown(true)}
-        {isShown && (  
-                    )} 
-             */
 export interface InfoHeaderProps extends HeadingProps {
     /* Text that will be displayed to the left of the icon */ 
     infotext: String;
@@ -21,13 +14,12 @@ export const InfoHeader: React.FC<InfoHeaderProps>  = ({
     ...props
 }): React.ReactElement => (
     <InfoHeaderText {...props}>
-        {infotext}
-        {showIcon(infotext)}
+      {showIcon(infotext)}
     </InfoHeaderText>
 );
 
 /**
-* This function takes in infotext and determines if the user is mousing over it 
+* This function determines if the user is mousing over the icon
 * If the user is the funtion will return the icon as well  as the text 
 * Otherwise just the text is shown 
 * @param {String} text - The time remaining in minutes  
@@ -40,11 +32,12 @@ const showIcon = (text:String) => {
         <InfoHeaderText
           onMouseEnter={() => setIsShown(true)}
           onMouseLeave={() => setIsShown(false)}>
+          {text}
           <Icon />
           {isShown && (
-        <div>
+        <ToolTipText>
           Tooltip
-        </div>
+        </ToolTipText>
       )}
         </InfoHeaderText>
     );
@@ -52,6 +45,11 @@ const showIcon = (text:String) => {
 
 const InfoHeaderText = styled(Heading)`
     text-align: left;
+`; 
+
+const ToolTipText = styled(Heading)`
+    text-align: left;
+    font-size: 70%;
 `; 
 
 const Icon = styled(InfoCircleFill)`
