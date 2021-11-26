885545c57c9db56fd89a170a1b08b29075810ccb
Hunter Fearnley <90480884+Hunterjdf@users.noreply.github.com>
2021-11-26 17:30:17 -0500

    Update InfoHeader.tsx

--- src/Containers/InfoHeader/InfoHeader.tsx
+++ src/Containers/InfoHeader/InfoHeader.tsx
@@ -8,34 +8,48 @@ export interface InfoHeaderProps extends HeadingProps {
     infotext: String;
   /* Text that will be displayed underneath the infotext when icon is hovered over */ 
     tooltiptext: String;
+  /* Action that triggers when mouse hovers over icon */
+    onMouseEnter: () => void;
+  /* Action that triggers when mouse stops hovering over icon */
+    onMouseLeave: () => void;    
     
 }
 
 export const InfoHeader: React.FC<InfoHeaderProps>  = ({
     infotext,
     tooltiptext,
+    onMouseEnter,
+    onMouseLeave,
     ...props
-}): React.ReactElement => (
-    <InfoHeaderText {...props}>
-      {showIcon(infotext,tooltiptext)}
-    </InfoHeaderText>
-);
-
-/**
-* This function determines if the user is mousing over the icon
-* If the user mousing over the icon, a tooltip is displayed 
-* Otherwise just the text and icon is shown 
-* @param {String} text - the infotext from main react element 
-* @param {String} tooltip - the tooltip that will be displayed  
-* @returns {InfoHeaderText} - The text, icon and a tootip 
-*/
-const showIcon = (text:String,tooltip:String) => {
+}): React.ReactElement => {
+  /**
+  * This function determines if the user is mousing over the icon
+  * If the user mousing over the icon, a tooltip is displayed 
+  * Otherwise just the text and icon is shown 
+  * @param {String} text - the infotext from main react element 
+  * @param {String} tooltip - the tooltip that will be displayed  
+  * @returns {InfoHeaderText} - The text, icon and a tootip 
+  */
+  const showIcon = (text:String,tooltip:String) => {
     const [isShown, setIsShown] = useState(false);
-    
+    /**
+     * This function reveals the tooltip when the mouse hovers over
+     */
+    const _onMouseEnter = () => {
+      setIsShown(true)
+      onMouseEnter()
+    }
+    /**
+     * This function hides the tooltip when the mouse leaves
+     */
+    const _onMouseLeave = () => {
+      setIsShown(false)
+      onMouseLeave()
+    }
     return (
         <InfoHeaderText
-          onMouseEnter={() => setIsShown(true)}
-          onMouseLeave={() => setIsShown(false)}>
+          onMouseEnter = {_onMouseEnter}
+          onMouseLeave={_onMouseLeave}>
           {text}
           <Icon />
           {isShown && (
@@ -46,6 +60,11 @@ const showIcon = (text:String,tooltip:String) => {
         </InfoHeaderText>
     );
   }
+
+  return  <InfoHeaderText {...props}>
+      {showIcon(infotext,tooltiptext)}
+    </InfoHeaderText>
+};
 
 const InfoHeaderText = styled(Heading)`
     text-align: left;
