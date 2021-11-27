1723a2910be2cd4f14ff86391d27f8f468598301
ralph-dev <ralphpal@hotmail.com>
2021-11-27 16:08:56 -0500

    Added Accessiblity Plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,7 +10,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-postcss"
+    "@storybook/addon-postcss",
+    "@storybook/addon-a11y"
   ],
   "features": {
     "babelModeV7": true,
--- src/Inputs/SearchBarExpandable/SearchBarExpandable.stories.tsx
+++ src/Inputs/SearchBarExpandable/SearchBarExpandable.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Meta, Story } from '@storybook/react';
-import { SearchBarExpandable, SearchBarExpandableProps } from '..';
+import { SearchBarExpandable, SearchBarExpandableProps } from '../../index';
 
 export default {
     title: 'Components/Search Bar Expandable',
