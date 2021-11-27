292121f1e4690dca017315f14e751db30669c157
ralph-dev <ralphpal@hotmail.com>
2021-11-27 12:28:20 -0500

    Migration to Storybook V6.4 Part 1 - WebPack Additions

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
+  "core": {
+    "builder": 'webpack5',
+  },
+  "framework": '@storybook/react',
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
@@ -7,5 +11,21 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-postcss"
-  ]
+  ],
+  "features": {
+    "babelModeV7": true
+  },
+  "webpackFinal": config => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        modules: ["../src", ...config.resolve.modules],
+        fallback: { 
+          "stream": require.resolve("stream-browserify"), 
+          ...config.resolve.fallback 
+        }
+      }
+    }
+  }
 }
