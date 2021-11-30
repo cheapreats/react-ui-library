3cff25e078b21ec9e7c77d453a511cc1aac35c48
ralph-dev <ralphpal@hotmail.com>
2021-11-30 01:20:48 -0500

    Added Roll Up Back for Bundling, still using latest webpack for compilation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,70 +1,70 @@
 const {
-  aliases
+    aliases
 } = require('./package.json');
 
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-    jest: true
-  },
-  extends: ["airbnb", "plugin:@typescript-eslint/recommended", "plugin:import/errors", "plugin:import/warnings", "plugin:import/typescript", "prettier", "plugin:storybook/recommended"],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parser: '@typescript-eslint/parser',
-  settings: {
-    'import/resolver': {
-      alias: {
-        map: Object.entries(aliases).map(item => {
-          item[1] = `./${item[1]}`;
-          return item;
-        }),
-        extensions: ['.ts', '.tsx', '.json', '.js']
-      }
-    }
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+        jest: true
     },
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
-  plugins: ['@typescript-eslint', 'react'],
-  rules: {
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/no-shadow': 'error',
-    '@typescript-eslint/ban-types': ['off'],
-    'react/jsx-filename-extension': ['error', {
-      extensions: ['.tsx']
-    }],
-    'react/jsx-indent-props': ['error', 4],
-    'react/jsx-indent': ['error', 4],
-    'react/jsx-props-no-spreading': ['off'],
-    'react/prop-types': 'off',
-    'import/namespace': ['error', {
-      allowComputed: true
-    }],
-    'import/prefer-default-export': 'off',
-    'import/extensions': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    camelcase: 'off',
-    'no-shadow': 'off',
-    'no-use-before-define': [0],
-    'array-callback-return': 'off',
-    'no-underscore-dangle': 'off',
-    'no-param-reassign': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'react/destructuring-assignment': 'off',
-    'no-console': 'off',
-    'import/no-named-as-default': 'off',
-    'no-alert': 'off',
-    indent: ['error', 4],
-    'react/require-default-props': 'off'
-  }
+    extends: ["airbnb", "plugin:@typescript-eslint/recommended", "plugin:import/errors", "plugin:import/warnings", "plugin:import/typescript", "prettier", "plugin:storybook/recommended"],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parser: '@typescript-eslint/parser',
+    settings: {
+        'import/resolver': {
+            alias: {
+                map: Object.entries(aliases).map(item => {
+                    item[1] = `./${item[1]}`;
+                    return item;
+                }),
+                extensions: ['.ts', '.tsx', '.json', '.js']
+            }
+        }
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint', 'react'],
+    rules: {
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-shadow': 'error',
+        '@typescript-eslint/ban-types': ['off'],
+        'react/jsx-filename-extension': ['error', {
+            extensions: ['.tsx']
+        }],
+        'react/jsx-indent-props': ['error', 4],
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-props-no-spreading': ['off'],
+        'react/prop-types': 'off',
+        'import/namespace': ['error', {
+            allowComputed: true
+        }],
+        'import/prefer-default-export': 'off',
+        'import/extensions': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        camelcase: 'off',
+        'no-shadow': 'off',
+        'no-use-before-define': [0],
+        'array-callback-return': 'off',
+        'no-underscore-dangle': 'off',
+        'no-param-reassign': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'react/destructuring-assignment': 'off',
+        'no-console': 'off',
+        'import/no-named-as-default': 'off',
+        'no-alert': 'off',
+        indent: ['error', 4],
+        'react/require-default-props': 'off'
+    }
 };
---
+++ rollup.config.js
@@ -0,0 +1,79 @@
+import tsPlugin from 'rollup-plugin-typescript2';
+import ttypescript from 'ttypescript';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+import json from '@rollup/plugin-json';
+import { DEFAULT_EXTENSIONS } from '@babel/core';
+import copy from 'rollup-plugin-copy';
+import pkg from './package.json';
+
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            name: '@cheapreats/react-ui',
+            file: pkg.main,
+            format: 'cjs',
+            sourcemap: true,
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+        },
+    ],
+    plugins: [
+        copy({
+            targets: [
+                { src: 'src/Containers/FileUpload/worker.js', dest: 'dist' },
+            ],
+        }),
+        commonjs(),
+        json(),
+        tsPlugin({
+            typescript: ttypescript,
+            tsconfig: './tsconfig.json',
+            include: ['*.ts+(|x)', '**/*.ts+(|x)'],
+            exclude: [
+                '*.d.ts',
+                '**/*.d.ts',
+                'node_modules/**',
+                '__tests__',
+                '.vscode',
+                '.github',
+                'scripts',
+                'dist',
+            ],
+            tsconfigOverride: { compilerOptions: { module: 'es2015' } },
+            tsconfigDefaults: {
+                compilerOptions: {
+                    plugins: [{ transform: '@zerollup/ts-transform-paths' }],
+                },
+            },
+        }),
+        nodeResolve({ preferBuiltins: true }),
+        babel({
+            exclude: ['node_modules', 'scripts', 'dist', '*.d.ts', '**/*.d.ts'],
+            extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx'],
+            plugins: [
+                'babel-plugin-styled-components',
+                'babel-plugin-transform-class-properties',
+            ],
+            presets: [
+                '@babel/preset-env',
+                '@babel/preset-react',
+                '@babel/preset-typescript',
+            ],
+            babelHelpers: 'bundled',
+        }),
+        postcss({
+            extensions: ['.css'],
+        }),
+    ],
+    external: [
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {}),
+        'workerize-loader',
+    ],
+};
