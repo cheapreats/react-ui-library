6f49ccb89fd70d7aaab0a43f56b0a1633389fb75
ralph-dev <ralphpal@hotmail.com>
2021-12-12 12:14:27 -0500

    Fixed Lib for Publishing

--- src/Containers/StoreSelector/StoreSelector.tsx
+++ src/Containers/StoreSelector/StoreSelector.tsx
@@ -52,7 +52,7 @@ export const StoreSelector: React.FC<StoreSelectorProps> = ({
 }): React.ReactElement => {
     const [currentIndex, setCurrentIndex] = useState(0);
     /* Gets the props for the current store by comparing its storeSelectorId to the current index state */
-    const currentCardObject = storeProfileData.find(storeProfileData => storeProfileData.storeSelectorId === currentIndex);
+    const currentCardObject = storeProfileData.find(storeProfile => storeProfile.storeSelectorId === currentIndex);
     /**
     * Takes the state, and increases the value by 1 conditionally. 
     * If the value is equal to the size of the array, then the value gets set to the bottom of the array instead (loops back around)
@@ -60,13 +60,13 @@ export const StoreSelector: React.FC<StoreSelectorProps> = ({
     * @param setCurrentIndex - Function to update currentIndex stateful value
     * @param storeProfileData - Array that currentIndex is used on
     */
-    let indexIncrease = (currentIndex: number, setCurrentIndex: Function, storeProfileData: Array<StoreSelectorArrayProps>) => {
+    const indexIncrease = () => {
         let indexIncreaseVar = currentIndex;
-        (
-            (indexIncreaseVar === storeProfileData.length - 1)
-                ? indexIncreaseVar -= storeProfileData.length - 1
-                : indexIncreaseVar += 1
-        )
+        if (indexIncreaseVar === storeProfileData.length - 1) {
+            indexIncreaseVar -= storeProfileData.length - 1
+        } else {
+            indexIncreaseVar += 1   
+        }
         setCurrentIndex(indexIncreaseVar);
     }
     /**
@@ -76,45 +76,45 @@ export const StoreSelector: React.FC<StoreSelectorProps> = ({
      * @param setCurrentIndex - Function to update that stateful value
      * @param storeProfileData - Array that currentIndex is used on
      */
-    let indexDecrease = (currentIndex: number, setCurrentIndex: Function, storeProfileData: Array<StoreSelectorArrayProps>) => {
+    const indexDecrease = () => {
         let indexDecreaseVar = currentIndex;
-        (
-            (indexDecreaseVar === 0)
-                ? indexDecreaseVar += storeProfileData.length - 1
-                : indexDecreaseVar -= 1
-        )
+        if (indexDecreaseVar === 0) {
+            indexDecreaseVar += storeProfileData.length - 1
+        } else {
+            indexDecreaseVar -= 1
+        }
         setCurrentIndex(indexDecreaseVar);
     }
     /**
     * Returns text based on vendorApprovalStatus of the array. 
     * @param storeBorderStatus - Value of current object's VendorApprovalStatus 
     */
-    let storeAccessibilitySwitch = (storeBorderStatus: VendorApprovalStatus) => {
+    const storeAccessibilitySwitch = (storeBorderStatus: VendorApprovalStatus | undefined) => {
         switch (storeBorderStatus) {
-            case VendorApprovalStatus.APPROVED:
-                return "Approved!";
-            case VendorApprovalStatus.PENDING:
-                return "Pending";
-            default:
-                return "Not Approved";
+        case VendorApprovalStatus.APPROVED:
+            return "Approved!";
+        case VendorApprovalStatus.PENDING:
+            return "Pending";
+        default:
+            return "Not Approved";
         }
     }
     return (
         <StoreSelectorBox {...props}>
             <StoreTextFlex>
-                <StoreSelectorText>{currentCardObject!.storeSelectorName}</StoreSelectorText>
+                <StoreSelectorText>{currentCardObject?.storeSelectorName}</StoreSelectorText>
                 <StoreSelectorTextFaded>Super Dashboard</StoreSelectorTextFaded>
-                <StoreAccessibilityText>Store: {currentIndex}. Status: {storeAccessibilitySwitch(currentCardObject!.storeStatus)} </StoreAccessibilityText>
+                <StoreAccessibilityText>Store: {currentIndex}. Status: {storeAccessibilitySwitch(currentCardObject?.storeStatus)} </StoreAccessibilityText>
             </StoreTextFlex>
-            <StoreSelectorBorder storeProfileData={storeProfileData} storeBorderStatus={currentCardObject!.storeStatus} />
+            <StoreSelectorBorder storeProfileData={storeProfileData} storeBorderStatus={currentCardObject?.storeStatus} />
             <ShopUser />
             <ArrowsDiv downArrowEnabled={downArrowEnabled} upArrowEnabled={upArrowEnabled}
                 storeProfileData={storeProfileData} onClick={downArrowEnabled || upArrowEnabled ? undefined : onClickArrows}>
                 <UpArrowDiv upArrowEnabled={upArrowEnabled} storeProfileData={storeProfileData} onClick={onClickUp}>
-                    <SelectorArrowUp onClick={() => indexIncrease(currentIndex, setCurrentIndex, storeProfileData)} />
+                    <SelectorArrowUp onClick={() => indexIncrease()} />
                 </UpArrowDiv>
                 <DownArrowDiv downArrowEnabled={downArrowEnabled} storeProfileData={storeProfileData} onClick={onClickDown}>
-                    <SelectorArrowDown onClick={() => indexDecrease(currentIndex, setCurrentIndex, storeProfileData)} />
+                    <SelectorArrowDown onClick={() => indexDecrease()} />
                 </DownArrowDiv>
             </ArrowsDiv>
         </StoreSelectorBox>
@@ -133,7 +133,8 @@ const StoreSelectorBox = styled.div`
         background-color: ${theme.colors.background};
         border-color: ${theme.colors.border};
     `}
-`
+`;
+
 const StoreSelectorBorder = styled.div<StoreSelectorProps>`
     width: 70px;
     height: 70px;
@@ -145,16 +146,17 @@ const StoreSelectorBorder = styled.div<StoreSelectorProps>`
     border-radius: 15px;
     border-color: ${({ theme, storeBorderStatus }): string => {
         switch (storeBorderStatus) {
-            case VendorApprovalStatus.APPROVED:
-                return theme.colors.reachIndicatorColors.green
-            case VendorApprovalStatus.PENDING:
-                return theme.colors.reachIndicatorColors.yellow
-            default:
-                return theme.colors.reachIndicatorColors.red
-            }
+        case VendorApprovalStatus.APPROVED:
+            return theme.colors.reachIndicatorColors.green
+        case VendorApprovalStatus.PENDING:
+            return theme.colors.reachIndicatorColors.yellow
+        default:
+            return theme.colors.reachIndicatorColors.red
         }
-    };
-`
+    }
+};
+`;
+
 const ArrowsDiv = styled.div<StoreSelectorProps>`
     padding: 5px;
     right: 10px;
@@ -166,48 +168,57 @@ const ArrowsDiv = styled.div<StoreSelectorProps>`
     padding-top: 5px;
     padding-bottom: 5px;
     };
-`
+`;
+
 const UpArrowDiv = styled.div<StoreSelectorProps>`
-    pointer-events: ${StoreSelector => StoreSelector.upArrowEnabled ? `auto` : `none` };
-`
+    pointer-events: ${({upArrowEnabled}) => upArrowEnabled ? `auto` : `none` };
+`;
+
 const SelectorArrowUp = styled(ChevronUp)`
     height: 25px;
     width: 25px;
-`
+`;
+
 const DownArrowDiv = styled.div<StoreSelectorProps>`
-     pointer-events: ${StoreSelector => StoreSelector.downArrowEnabled ? `auto` : `none` }; 
-`
+     pointer-events: ${({downArrowEnabled}) => downArrowEnabled ? `auto` : `none` }; 
+`;
+
 const SelectorArrowDown = styled(ChevronDown)`
     height: 25px;
     width: 25px;
-`
+`;
+
 const StoreTextFlex = styled.div`
     padding: 20px 45px 10px 100px;
     display: flex;
     flex-direction: column;
     justify-content: center;
-`
+`;
+
 const StoreSelectorText = styled.header`
     font-weight: bold;
     font-size: 20px;
     ${({ theme }): string => `
         color: ${theme.colors.text}
     `}
-`
+`;
+
 const StoreSelectorTextFaded = styled(StoreSelectorText)`
     font-size: 12px;
     ${({ theme }): string => `
         color: ${theme.colors.bannerBackgroundColor}
     `}
-`
+`;
+
 const StoreAccessibilityText = styled(StoreSelectorText)`
     font-size: 13px;
-`
+`;
+
 const ShopUser = styled(PersonSquare) `
     width: 60px;
     height: 60px;
     position: absolute;
     top: 20px;
     left: 20px;
-`
+`;
 
