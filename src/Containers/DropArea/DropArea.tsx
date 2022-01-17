18a543bb2176a8a69bb0727315dd911bfed6ef76
roggc <roggc9@gmail.com>
2022-01-17 19:06:02 +0100

    add story isDragEnter

--- src/Containers/DropArea/DropArea.tsx
+++ src/Containers/DropArea/DropArea.tsx
@@ -37,6 +37,20 @@ border-radius:${theme.dimensions.radius};
 ${
     isDragEnter
         ? `
+        background-color:#cce6ff;
+        @keyframes border-dance {
+            0% {
+              background-position: left top, right bottom, left bottom, right   top;
+            }
+            100% {
+              background-position: left 15px top, right 15px bottom , left bottom 15px , right   top 15px;
+            }
+          }
+          background-image: linear-gradient(90deg, #3399ff 50%, transparent 50%), linear-gradient(90deg, #3399ff 50%, transparent 50%), linear-gradient(0deg, #3399ff 50%, transparent 50%), linear-gradient(0deg, #3399ff 50%, transparent 50%);
+          background-repeat: repeat-x, repeat-x, repeat-y, repeat-y;
+          background-size: 15px 2px, 15px 2px, 2px 15px, 2px 15px;
+          background-position: left top, right bottom, left bottom, right   top;
+          animation: border-dance .3s infinite linear;
 `
         : `
 border:2px dashed grey;
