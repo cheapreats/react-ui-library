105dcdbe5baa012489d40921c29b33f1363543f5
roggc <roggc9@gmail.com>
2022-01-20 20:26:15 +0100

    working...

--- src/Containers/PanelCard/PanelCard.tsx
+++ src/Containers/PanelCard/PanelCard.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+import { StyledIcon } from 'styled-icons/types';
 import { Button } from '@Inputs/Button/Button';
 import { CardProps, Card } from '../Card/Card';
+
+enum OperationState{
+    isFailure,
+    isSuccess,
+    isLoading,
+    isUnknown,
+}
 
 export interface IPanelCardProps extends CardProps {
     /** callback for cancel loading file */
     onCancelLoading?: () => void;
+    name?:string;
     isFailureMessage?: string;
     isSuccessMessage?: string;
     isLoadingMessage?: string;
-    cancelLoadingText?: string;
+    cancelLoadingButtonText?: string;
+    operationState?:OperationState;
+    isSuccessIcon?:StyledIcon;
+    isFailureIcon?:StyledIcon;
+    /** icons width and height */
+    iconDimensions?:number;
 }
 
 export const PanelCard: React.FC<IPanelCardProps> = ({
@@ -16,12 +31,20 @@ export const PanelCard: React.FC<IPanelCardProps> = ({
     isFailureMessage = '',
     isLoadingMessage = '',
     isSuccessMessage = '',
-    cancelLoadingText = 'Cancel',
+    cancelLoadingButtonText = 'Cancel',
+    name='',
+    operationState=OperationState.isUnknown,
+    isSuccessIcon=,
+    isFailureIcon=,
     ...props
 }): React.ReactElement => {
     const renderContent=useCallback(()=>{
+        switch(operationState){
+            case OperationState.isFailure:
+                return 
+        }
 
-    },[isFailureMessage,isLoadingMessage,isSuccessMessage])
+    },[isFailureMessage,isLoadingMessage,isSuccessMessage,operationState])
     return (
         <Card {...props}>
             <div>{isFailureMessage || isLoadingMessage || isSuccessMessage}</div>
@@ -31,3 +54,8 @@ export const PanelCard: React.FC<IPanelCardProps> = ({
         </Card>
     );
 }
+
+const Icon=styled.svg`
+width:60px;
+height:60px;
+`
