e7901753234a5d8ef309628b20c281f100c34ded
ericlin11354 <ericlin11354@gmail.com>
2022-01-22 01:27:56 -0500

    Added JSDocs. Exposed Dropdown Props. Renamed 'options' to 'dropDownOptions'

--- src/Containers/SpecialText/SpecialText.tsx
+++ src/Containers/SpecialText/SpecialText.tsx
@@ -1,44 +1,49 @@
-import Dropdown from '../Dropdown/Dropdown';
+import Dropdown, { IDropdownProps } from '../Dropdown/Dropdown';
 import DropdownItem from '../Dropdown/DropdownItem';
 import React, { Children, isValidElement, useCallback, useState } from 'react';
 import { SmallText } from 'index';
 import styled from 'styled-components';
 
+/**
+ * Properties for SpecialText
+ * @param {string} text - Highlighted text
+ * @param {React.ReactNode[]} children - List of elements in DropDown
+ * @param {IDropdownProps} dropDownProps
+ */
 export interface SpecialTextProps {
     text: string;
     children: React.ReactNode[];
+    dropDownProps: IDropdownProps[];
 }
 
-const formatList = (
-    children: React.ReactNode[]
-    ): React.ReactNode[] => 
+/**
+ * Maps SpecialText children to DropdownItems in DropDown
+ * @param {React.ReactNode[0]} children - Elements in the DropDown
+ * @returns {React.ReactNode[]} - Array of valid DropdownItems
+ */
+const formatList = (children: React.ReactNode[]): React.ReactNode[] =>
     children.map((child): React.ReactElement | null => {
         if (child && isValidElement(child)) {
             const val = child.props.value;
-            return (
-                <DropdownItem 
-                    {...child.props}
-                    children={child} 
-                    key={val} 
-                    />
-            );
-        }
-        else return null;
+            return <DropdownItem {...child.props} children={child} key={val} />;
+        } else return null;
     });
 
 export const SpecialText: React.FC<SpecialTextProps> = ({
     text,
     children,
+    dropDownProps,
     ...props
 }): React.ReactElement => {
-    const options = Children.toArray(children)
+    const dropDownOptions = Children.toArray(children);
 
     return (
         <StyledDiv {...props}>
-            <Dropdown dropdownButton={
-                <StyledDropDownText>{text}</StyledDropDownText>
-            }>
-                {formatList(options)}
+            <Dropdown
+                dropdownButton={<StyledDropDownText>{text}</StyledDropDownText>}
+                {...dropDownProps}
+            >
+                {formatList(dropDownOptions)}
             </Dropdown>
         </StyledDiv>
     );
