fb55f6c7a89a89d1b6775f1584c488ef533fc3ff
roggc <roggc9@gmail.com>
2022-01-23 00:48:37 +0100

    add fadeout effect

---
+++ src/Containers/PanelListWrapper/useFadeOut.ts
@@ -0,0 +1,59 @@
+import { useEffect, useState, useRef, Dispatch, SetStateAction } from 'react';
+import { IPanelCardProps } from '../PanelCard/PanelCard';
+
+export const useFadeOut = (
+    panels: IPanelCardProps[],
+): readonly [
+    {
+        panel: IPanelCardProps;
+        isShown: boolean;
+    }[],
+    Dispatch<
+        SetStateAction<
+            {
+                panel: IPanelCardProps;
+                isShown: boolean;
+            }[]
+        >
+    >,
+] => {
+    const previousPanelsAmmount = useRef<number>();
+    const [internalPanels, setInternalPanels] = useState(
+        panels.map((panel) => ({ panel, isShown: true })),
+    );
+
+    useEffect(() => {
+        if (
+            previousPanelsAmmount.current === undefined ||
+            previousPanelsAmmount.current > panels.length
+        ) {
+            setInternalPanels((prev) =>
+                prev.map((internalPanel) => {
+                    if (
+                        !panels.some(
+                            (panel) => panel.name === internalPanel.panel.name,
+                        )
+                    ) {
+                        return { ...internalPanel, isShown: false };
+                    }
+                    return internalPanel;
+                }),
+            );
+        } else {
+            const panelsToAdd = panels.filter(
+                (panel) =>
+                    !internalPanels.some(
+                        (internalPanel) =>
+                            internalPanel.panel.name === panel.name,
+                    ),
+            );
+            setInternalPanels((prev) => [
+                ...prev,
+                ...panelsToAdd.map((panel) => ({ panel, isShown: true })),
+            ]);
+        }
+        previousPanelsAmmount.current = panels.length;
+    }, [panels]);
+
+    return [internalPanels, setInternalPanels] as const;
+};
