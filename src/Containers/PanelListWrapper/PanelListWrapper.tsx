536eccbf7e510801787a434ea185fd9c93134f61
roggc <roggc9@gmail.com>
2022-01-23 00:48:37 +0100

    add fadeout effect

--- src/Containers/PanelListWrapper/PanelListWrapper.tsx
+++ src/Containers/PanelListWrapper/PanelListWrapper.tsx
@@ -3,6 +3,7 @@ import React, { useMemo } from 'react';
 import styled from 'styled-components';
 import { PanelCard, IPanelCardProps } from '../PanelCard/PanelCard';
 import { useSequentiallyAddedPanels } from './useSequentiallyAddedPanels';
+import { useFadeOut } from './useFadeOut';
 
 export interface IPanelListWrapperProps
     extends MainInterface,
@@ -15,7 +16,7 @@ export interface IPanelListWrapperProps
     /** dealy in ms for each panel added to the list */
     delaySequentially?: number;
     /** fade in in ms */
-    fadeIn?:number;
+    fadeIn?: number;
 }
 
 export const PanelListWrapper: React.FC<IPanelListWrapperProps> = ({
@@ -23,7 +24,7 @@ export const PanelListWrapper: React.FC<IPanelListWrapperProps> = ({
     verticalSpacing,
     isAddPanelsSequentially = false,
     delaySequentially = 100,
-    fadeIn=1000,
+    fadeIn = 1000,
     ...props
 }) => {
     const sequentiallyPanels = useSequentiallyAddedPanels(
@@ -35,15 +36,24 @@ export const PanelListWrapper: React.FC<IPanelListWrapperProps> = ({
             isAddPanelsSequentially ? sequentiallyPanels : panels,
         [isAddPanelsSequentially, sequentiallyPanels, panels],
     );
+    const [fadeOutPanels, setFadeOutPanels] = useFadeOut(panelsToMap);
+
+    const removePanelsIsShownIsFalse = () => {
+        setFadeOutPanels((prev) =>
+            prev.filter((fadeOutPanel) => fadeOutPanel.isShown),
+        );
+    };
 
     return (
         <PanelListWrapperBox {...props}>
-            {panelsToMap.map((panel) => (
-                <PanelCardFadeIn
+            {fadeOutPanels.map(({ panel, isShown }) => (
+                <PanelCardFadeOut
                     key={`${panel.name}`}
                     {...panel}
                     margin={`${verticalSpacing}px 0`}
                     fadeIn={fadeIn}
+                    isShown={isShown}
+                    onAnimationEnd={removePanelsIsShownIsFalse}
                 />
             ))}
         </PanelListWrapperBox>
@@ -54,10 +64,28 @@ const PanelListWrapperBox = styled.div<MainInterface>`
     ${(props) => Main({ ...props })}
 `;
 
-const PanelCardFadeIn=styled(PanelCard)<{fadeIn:number}>`
-@keyframes fadein{
-    from{opacity:0;}
-    to{opacity:1;}
-}
-animation:fadein ${({fadeIn})=>fadeIn}ms;
-`
+const PanelCardFadeIn = styled(PanelCard)<{ fadeIn: number }>`
+    @keyframes fadein {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+    animation: fadein ${({ fadeIn }) => fadeIn}ms;
+`;
+
+const PanelCardFadeOut = styled(PanelCardFadeIn)<{ isShown: boolean }>`
+    @keyframes fadeout {
+        from {
+            opacity: 1;
+        }
+        to {
+            opacity: 0;
+        }
+    }
+    ${({ isShown }) => `
+${isShown ? '' : 'animation:fadeout 1000ms;'}
+`}
+`;
