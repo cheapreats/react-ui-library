2fdc6c04aa45917a1f6a84fa6cec1388dc1e0f0b
roggc <roggc9@gmail.com>
2022-01-23 19:42:35 +0100

    fill worker.ts

---
+++ src/Containers/FileUploadV2/worker.ts
@@ -0,0 +1,24 @@
+onmessage = (e) => {
+    const { file } = e.data;
+    const reader = new FileReader();
+    reader.onload = () => {
+        let base64StringFile = 'NO_BASE64STRINGFILE';
+        if (reader.result) {
+            if (typeof reader.result === 'string') {
+                base64StringFile = btoa(reader.result);
+            } else {
+                const bytes = Array.from(new Uint8Array(reader.result));
+                base64StringFile = btoa(
+                    bytes.map((item) => String.fromCharCode(item)).join(''),
+                );
+            }
+        }
+        postMessage({ base64StringFile,name:file.name });
+    };
+    try{
+        reader.readAsArrayBuffer(file);
+    }catch(error){
+        console.log(error);
+        postMessage({});
+    }
+};
