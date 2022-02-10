3f5ea82e73f230596af41494507dfac8843b24d6
Yurii <iamyuriizhuk@gmail.com>
2022-02-09 19:27:45 -0800

    Add AdvancedChair component

---
+++ src/Containers/AdvancedChair/AdvancedChair.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { AdvancedChair, IAdvancedChair } from './AdvancedChair';
+import { createStoryTitle } from '../../Constants';
+import {action} from "@storybook/addon-actions";
+
+export default {
+    title: createStoryTitle('AdvancedChair'),
+    component: AdvancedChair,
+} as Meta;
+
+const Template: Story<IAdvancedChair> = (args) => <AdvancedChair {...args}/>;
+
+export const VacantChair = Template.bind({});
+VacantChair.args = {
+    position: 'right',
+    relativeSize: 0.5,
+    occupiedBy: 'JS',
+    onChairClick: action('The chair is clicked'),
+    chairLegProps: {onClick: () => console.log('hello'), style:{backgroundColor:"pink"}}
+};
+
+export const PersonalizedChair = Template.bind({});
+PersonalizedChair.args = {
+    position: 'right',
+    relativeSize: 0.5,
+    occupiedBy: 'YZ',
+    onChairClick: action('The chair is clicked'),
+    chairLegProps: {onClick: () => console.log('hello'), style:{borderRadius: 0}}
+};
